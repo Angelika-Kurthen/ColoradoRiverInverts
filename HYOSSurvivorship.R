2c85fe9d69f1510944bb1c04e76d0baa8bc0d2ff
Angelika Kurthen <kurthena@oregonstate.edu>
2022-12-21 15:41:15 -0800

    Created code to fit negative exponential fx to data points, rooting survival at Qmin = 100%

---
+++ HYOSSurvivorship.R
@@ -0,0 +1,38 @@
+#####################################
+## Code to fit curve to Hydropsyche spp mortality rates to data
+####################################
+library(readxl)
+library(minpack.lm)
+library(tidyverse)
+HYOSVitalRates <- read_excel("VitalRates.xlsx", sheet = "Hydropsyche Mortality Rates")
+HYOSVitalRates <- as.data.frame(HYOSVitalRates)
+
+flow.surv.fit <- function(magnitude, mortality, Qmin){
+  x <- magnitude # rename x and y
+  y <- 1 - mortality # convert to survival
+  df <- as.data.frame(cbind(x,y))
+  df <- rbind(df, c(Qmin, 1))  # make sure we specify that we have 100% survival at Qmin (0.25)
+  nls.fit <- nlsLM(formula = (y ~ k* exp(-h*x)), data = df, start = c(k = 1, h = 1)) # fit to negative exponential function
+  return(nls.fit)
+}
+
+flow.surv.rate <- function(h, k, max, min, interval, Qmin) {
+  Q <- seq(min, max, by = interval)
+  surv <- k*(exp(-h*Q))
+  surv.df <- as.data.frame(cbind(Q, surv))
+  surv.df$surv[which(surv.df$Q <= Qmin)] <- 1
+  return(surv.df)
+}
+
+surv.fit.HYOS <- flow.surv.fit(HYOSVitalRates$`Max Event Discharge/Bankfull Discharge`, HYOSVitalRates$Mortality, 0.25)
+surv.df.HYOS <- flow.surv.rate(surv.fit.HYOS$m$getPars()[2] , surv.fit.HYOS$m$getPars()[1], 2, 0.001, 0.001, 0.25)
+
+ggplot(surv.df.HYOS, aes(x = Q, y = surv))+
+  geom_line()+
+  geom_point(data = HYOSVitalRates, aes(x = `Max Event Discharge/Bankfull Discharge` , y = 1-(Mortality), color = Citation))+
+  #coord_cartesian(ylim = c(0,1)) +
+  ylab('Immediate Post-Disturbance Survival') +
+  theme_bw()+
+  xlab('`Max Event Discharge/Bankfull Discharge`')
+
+  
