b600d9d429deb935ec1c1b1069ff8e737d41daa1
Angelika Kurthen <kurthena@oregonstate.edu>
2023-01-19 17:20:12 -0800

    created function to calculate hydropeaking modifier

--- EggMortalityIntegration.R
+++ EggMortalityIntegration.R
@@ -4,7 +4,7 @@
 
 # Under high hydropeaking, egg survival follows eq. 7 in Kennedy et al., 2016
 
-f <- function(x) {exp(-r*2)*x*r}
+hydrofunction <- function(x) {exp(-x*c)}
 
 # where r is where in the river a species lays eggs (BAET = 0, CHIRO = 0.5, SIMU = 0.8)
 # c is a constant related to an egg survival curve (eq 2), in this case the c = 2
@@ -16,18 +16,24 @@ f <- function(x) {exp(-r*2)*x*r}
 # CHIRO have a middle strategy [0.4, 0.6]
 
 # high hydropeaking egg modifier for BAET
-r = 0
-integrate(f, lower = 0, upper = 0.2)
-H_BAET = 0.003230344*2
+h = 0.56
+# proportion of eggs lost to desiccation
+int_baet <- integrate(f, lower = 0, upper = 0.2)
+H_BAET = (1 - int_baet$value*2) *(1-h)
 
-# high hydropeaking egg modifier for CHIRO
-r = 0.5
-integrate(f, lower = 0.4, upper = 0.6)
-H_CHIRO = 0.01615172*2
+# high hydropeaking egg modifier for CHIRO (and HYOS?)
+h = 0.56
+int_chiro <- integrate(f, lower = 0.4, upper = 0.6)
+H_CHIRO = (1 - int_chiro$value*2) *(1-h)
 
 # high hydropeaking egg modifer for SIMU
-r = 0.8
-integrate(f, lower = 0.8, upper =1 )
-H_SIMU = 0.02677842*2
+h = 0.56
+int_simu <- integrate(f, lower = 0.8, upper =1 )
+H_SIMU = (1- int_simu$value*2) * (1-h)
 
 
+hydropeaking.mortality <- function(lower, upper, h){
+  int <- integrate(hydrofunction, lower = lower, upper = upper)
+  H <- (1 - int$value*2) * (1-h)
+  return(H)
+}
