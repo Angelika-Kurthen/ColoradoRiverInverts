a3ee5b95076ad4ced9cb95bf0715ab6cfb489a11
Angelika Kurthen <kurthena@oregonstate.edu>
2023-01-29 12:04:46 -0800

    Moved functions into 1spFunction area, now just a plotting tool

--- ColoradoRiverTempRamp.R
+++ ColoradoRiverTempRamp.R
@@ -1,10 +1,6 @@
 ###########################################
-## Code to create a ramping temp increase
+## Code to create a plot for a ramping temp increase at Lees Ferry
 ###########################################
-
-
-
-
 library(purrr)
 library(tidyverse)
 library(lubridate)
@@ -13,88 +9,23 @@ library(dplyr)
 library(ggplot2)
 # data retrieval tool from USGS
 library(dataRetrieval)
+#Lees Ferry Temps
+temp <- readNWISdv("09380000", "00010", "2007-10-01", "2021-09-30")
 
-flow.magnitude <- TimestepDischarge(flow, 85000) #discharge data from USGS is in cfs, bankfull dischage (pers comm TK) 85000 cfs
+# historical average for each 2 weeks timestep concatenated into a yearlong timeseries
+temps <- average.yearly.temp(temp, "X_00010_00003", "Date")
 
-temp <- temp <- readNWISdv("09380000", "00010", "2007-10-01", "2021-09-30")
+# n is number of years I want the final time series to have
+n = 13
+# qr is the temp ramps I want to increase the average Lees Ferry temp by 
+qr <- c(0, 1, 2.5, 5, 7.5)
+# how many years I want each temp ramp to last
+r <- c(5, 2, 2, 2, 2)
 
-temp$Date <- as_datetime(temp$Date)
-temp$Date <- yday(temp$Date)
+# manipulating temps to make a temp ramp
+temps <- rep.avg.year(temps, n, change.in.temp = qr, years.at.temp = r)
 
-
-# option 1: for each date, calculate mean and standard error so we can create a new dataset
-temp <- temp %>% group_by(Date) %>% dplyr::summarise(Temperature = mean(X_00010_00003), 
-                                                     sd = sd(X_00010_00003), 
-                                                     count = n(), 
-                                                     se = sd(X_00010_00003)/count)
-temp
-
-
-
-
-
-
-
-# Make an index to be used for aggregating
-ID <- as.numeric(as.factor(temp$Date)) 
-# want it to be every 14 days, hence the 14
-ID <- ID %/% 14
-
-ID[which(ID ==26)] <- 25
-# aggregate over ID and TYPEall numeric data.
-outs <- aggregate(temp[sapply(temp,is.numeric)],
-                  by=list(ID),
-                  FUN=mean)
-
-
-
-# format output
-names(outs)[2:3] <-c("dts","Temperature")
-# add the correct dates as the beginning of every period
-outs$dts <- strptime(round(outs$dts), "%j") ###Note need to subtract 365 if larger than 365
-
-# order by date in chronological order#
-#outs <- outs[order(outs$dts),]
-outs$dts <- as_date(outs$dts)
-
-n <- 13
-
-# repeat this data frame for 13 years
-temp_seq <- do.call("rbind", replicate(n, outs, simplify = FALSE))
-temp_seq$dts <- as.Date(temp_seq$dts)
-# now adjust the years so time can proceed in chronological order
-year(temp_seq$dts[1:26]) <- 2000
-year(temp_seq$dts[27:52]) <- 2001
-year(temp_seq$dts[53:79]) <- 2002
-year(temp_seq$dts[80:104]) <- 2003
-year(temp_seq$dts[105:130]) <- 2004
-year(temp_seq$dts[131:156]) <- 2005
-year(temp_seq$dts[157:182]) <- 2006
-year(temp_seq$dts[183:208]) <- 2007
-year(temp_seq$dts[209:234]) <- 2008
-year(temp_seq$dts[235:260]) <- 2009
-year(temp_seq$dts[261:286]) <- 2010
-year(temp_seq$dts[287:312]) <- 2011
-year(temp_seq$dts[313:338]) <- 2012
-
-
-# adjust the year temps
-
-# first five years remain at mean temp for 2007 - 2022, which we will call baseline
-
-# years 6 and 7  represent a 1 C increase from baseline
-temp_seq$Temperature[131:182] <- temp_seq$Temperature[131:182] + 1
-
-# years 8 and 9 represent a 2.5 C increase from baseline
-temp_seq$Temperature[183:234] <- temp_seq$Temperature[183:234] + 2.5
-
-# years 10 and 11 represent at 5 C increase from baseline
-temp_seq$Temperature[235:286] <- temp_seq$Temperature[235:286] + 5
-
-# year 11 and 12 represent a 7.5 C increase from baseline
-temp_seq$Temperature[287:338] <- temp_seq$Temperature[287:338] + 7.5
-
-
+# some code for plotting
 arrows <- tibble(
   x1 = c("2005-01-07", "2007-01-07", "2009-01-07", "2011-01-07"),
   x2 = c("2005-01-07", "2007-01-07", "2009-01-07", "2011-01-07"),
