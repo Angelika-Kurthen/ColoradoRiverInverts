91a9d37b190f7e8c4ed969e460ae1c78e774bd8e
Angelika Kurthen <kurthena@oregonstate.edu>
2023-01-29 14:06:07 -0800

    Added extinction threshold function

--- NZMS_1sp_Model_Hydropeak_ramped.R
+++ NZMS_1sp_Model_Hydropeak_ramped.R
@@ -27,7 +27,6 @@ library(dataRetrieval)
 # source functions
 source("NZMSSurvivorship.R")
 source("1spFunctions.R")
-source("ColoradoRiverTempRamp.R")
 # growth - according to _ growth is dependent on shell length, as is fecundity 
 # we want 1 class of non-reproductive subadults (smaller than 3.2 mm) and two classes of reproductive adults (3.2 mm and larger)
 # growth follows the equation growth per day = -0.006*length[t]+0.029 (Cross et al., 2010)
@@ -41,67 +40,14 @@ shell.growth(0.994, 0.029, 0.5)
 #read in flow data from USGS gauge at Lees Ferry, AZ between 1985 to the end of the last water year
 flow <- readNWISdv("09380000", "00060", "1985-10-01", "2021-09-30")
 temp <- temp <- readNWISdv("09380000", "00010", "2007-10-01", "2021-09-30")
-temp$Date <- as_datetime(temp$Date)
-temp$Date <- yday(temp$Date)
 
-
-# option 1: for each date, calculate mean and standard error so we can create a new dataset
-temp <- temp %>% group_by(Date) %>% dplyr::summarise(Temperature = mean(X_00010_00003), 
-                                                     sd = sd(X_00010_00003), 
-                                                     count = n(), 
-                                                     se = sd(X_00010_00003)/count)
-temp
-
-
-
-
-
-
-
-# Make an index to be used for aggregating
-ID <- as.numeric(as.factor(temp$Date)) 
-# want it to be every 14 days, hence the 14
-ID <- ID %/% 14
-
-ID[which(ID ==26)] <- 25
-# aggregate over ID and TYPEall numeric data.
-outs <- aggregate(temp[sapply(temp,is.numeric)],
-                  by=list(ID),
-                  FUN=mean)
-
-
-
-# format output
-names(outs)[2:3] <-c("dts","Temperature")
-# add the correct dates as the beginning of every period
-outs$dts <- strptime(round(outs$dts), "%j") ###Note need to subtract 365 if larger than 365
-
-# order by date in chronological order#
-#outs <- outs[order(outs$dts),]
-outs$dts <- as_date(outs$dts)
-
+temps <- average.yearly.temp(temp, "X_00010_00003", "Date")
 n <- 13
-
-# repeat this data frame for 13 years
-temp_seq <- do.call("rbind", replicate(n, outs, simplify = FALSE))
-temp_seq$dts <- as.Date(temp_seq$dts)
-temps <- temp_seq
-degreedays <- temps$Temperature * 14
-
-year(temp_seq$dts[1:26]) <- 2000
-year(temp_seq$dts[27:52]) <- 2001
-year(temp_seq$dts[53:79]) <- 2002
-year(temp_seq$dts[80:104]) <- 2003
-year(temp_seq$dts[105:130]) <- 2004
-year(temp_seq$dts[131:156]) <- 2005
-year(temp_seq$dts[157:182]) <- 2006
-year(temp_seq$dts[183:208]) <- 2007
-year(temp_seq$dts[209:234]) <- 2008
-year(temp_seq$dts[235:260]) <- 2009
-year(temp_seq$dts[261:286]) <- 2010
-year(temp_seq$dts[287:312]) <- 2011
-year(temp_seq$dts[313:338]) <- 2012
-
+# qr is the temp ramps I want to increase the average Lees Ferry temp by 
+qr <- c(0, 1, 2.5, 5, 7.5)
+# how many years I want each temp ramp to last
+r <- c(5, 2, 2, 2, 2)
+temps <- rep.avg.year(temps, n, change.in.temp = qr, years.at.temp = r)
 
 
 # specify iterations
@@ -162,7 +108,7 @@ g <- 0.1
 h <- surv.fit.NZMS$m$getPars()[2]   
 k <- surv.fit.NZMS$m$getPars()[1]
 e = 2.71828
-
+extinction <- 1
 
 #-------------------------
 # Outer Loop of Iterations
@@ -275,7 +221,8 @@ for (iter in c(1:iterations)) {
     #-------------------------------------------------
     # Calculate sum of all stages (total population)
     Total.N[,iter] <- apply(output.N.list[,,iter],1,sum)
-    
+    # Check extinction threshold 
+    extinction.threshold(extinction)
     
   } #-------------------------
   # End Inner Loop  
