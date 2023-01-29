5b17e4f7f5b57bb14678cea801a2eb652da41711
Angelika Kurthen <kurthena@oregonstate.edu>
2023-01-29 14:15:28 -0800

    added extinction threshold function

--- HYOS_1sp_Model_Ramped.R
+++ HYOS_1sp_Model_Ramped.R
@@ -28,79 +28,20 @@ library(dataRetrieval)
 
 source("HYOSSurvivorship.R")
 source("1spFunctions.R")
-source("ColoradoRiverTempRamp.R")
 
 #read in flow data from USGS gauge at Lees Ferry, AZ between 1985 to the end of the last water year
 flow <- readNWISdv("09380000", "00060", "1985-10-01", "2021-09-30")
 # read in temperature data from USGS gauge at Lees Ferry, AZ between _ to the end of last water year
 temp <- read.delim("gcmrc20230123125915.tsv", header=T)
-
-#---------------------------------------------------------------
-# this chunk of code makes the repeating avg ColRiv temp series
 colnames(temp) <- c("Date", "Temperature")
-temp$Date <- as_datetime(temp$Date)
-temp$Date <- yday(temp$Date)
-
-
-# option 1: for each date, calculate mean and standard error so we can create a new dataset
-temp <- temp %>% group_by(Date) %>% dplyr::summarise(Temperature = mean(Temperature))
-temp
-
-# Make an index to be used for aggregating
-ID <- as.numeric(as.factor(temp$Date)) 
-# want it to be every 14 days, hence the 14
-ID <- ID %/% 14
-
-ID[which(ID ==26)] <- 25
-# aggregate over ID and TYPEall numeric data.
-outs <- aggregate(temp[sapply(temp,is.numeric)],
-                  by=list(ID),
-                  FUN=mean)
-
-
-
-# format output
-names(outs)[2:3] <-c("dts","Temperature")
-# add the correct dates as the beginning of every period
-outs$dts <- strptime(round(outs$dts), "%j") ###Note need to subtract 365 if larger than 365
-
-# order by date in chronological order#
-#outs <- outs[order(outs$dts),]
-outs$dts <- as_date(outs$dts)
-
+temps <- average.yearly.temp(temp, "Temperature", "Date")
 n <- 13
+# qr is the temp ramps I want to increase the average Lees Ferry temp by 
+qr <- c(0, 1, 2.5, 5, 7.5)
+# how many years I want each temp ramp to last
+r <- c(5, 2, 2, 2, 2)
+temps <- rep.avg.year(temps, n, change.in.temp = qr, years.at.temp = r)
 
-# repeat this data frame for 13 years
-temp_seq <- do.call("rbind", replicate(n, outs, simplify = FALSE))
-temp_seq$dts <- as.Date(temp_seq$dts)
-# now adjust the years so time can proceed in chronological order
-year(temp_seq$dts[1:26]) <- 2000
-year(temp_seq$dts[27:52]) <- 2001
-year(temp_seq$dts[53:79]) <- 2002
-year(temp_seq$dts[80:104]) <- 2003
-year(temp_seq$dts[105:130]) <- 2004
-year(temp_seq$dts[131:156]) <- 2005
-year(temp_seq$dts[157:182]) <- 2006
-year(temp_seq$dts[183:208]) <- 2007
-year(temp_seq$dts[209:234]) <- 2008
-year(temp_seq$dts[235:260]) <- 2009
-year(temp_seq$dts[261:286]) <- 2010
-year(temp_seq$dts[287:312]) <- 2011
-year(temp_seq$dts[313:338]) <- 2012
-#--------------------------------------
-# years 6 and 7  represent a 1 C increase from baseline
-temp_seq$Temperature[131:182] <- temp_seq$Temperature[131:182] + 1
-
-# years 8 and 9 represent a 2.5 C increase from baseline
-temp_seq$Temperature[183:234] <- temp_seq$Temperature[183:234] + 2.5
-
-# years 10 and 11 represent at 5 C increase from baseline
-temp_seq$Temperature[235:286] <- temp_seq$Temperature[235:286] + 5
-
-# year 11 and 12 represent a 7.5 C increase from baseline
-temp_seq$Temperature[287:338] <- temp_seq$Temperature[287:338] + 7.5
-
-temps <- temp_seq
 degreedays <- as.data.frame(cbind(temp_seq$dts, temps$Temperature * 14))
 colnames(degreedays) <- c("dts", "DegreeDay")
 
@@ -162,7 +103,7 @@ a <- 0.1
 g <- 0.1
 h <- surv.fit.HYOS$m$getPars()[2]   
 k <- surv.fit.HYOS$m$getPars()[1] 
-
+extinction <- 500
 #-------------------------
 # Outer Loop of Iterations
 #--------------------------
@@ -304,6 +245,8 @@ for (iter in c(1:iterations)) {
     flowmortlist <- append(flowmortlist, flood.mortality(1, k, h, Q[t-1], Qmin))
     #replist[[1]][,,1] <- output.N.list[[1]]
     Total.N[,iter] <- apply(output.N.list[,,iter],1,sum)
+    # check extinction threshold
+    extinction.threshold(extinction)
   } #-------------------------
   # End Inner Loop  
   #------------------------- 
