1e7ac0fa761d43e5741790ad181165e2a9f8778b
Angelika Kurthen <kurthena@oregonstate.edu>
2023-02-03 19:18:08 -0800

    Tried to get an ok baet model

--- BAET_1sp_Model.R
+++ BAET_1sp_Model.R
@@ -29,19 +29,44 @@ library(dataRetrieval)
 source("BAETSurvivorship.R")
 source("1spFunctions.R")
 
-#read in flow data from USGS gauge at Lees Ferry, AZ between 1985 to the end of the last water year
-flow <- readNWISdv("09380000", "00060", "1985-10-01", "2021-09-30")
-# read in temperature data from USGS gauge at Lees Ferry, AZ between _ to the end of last water year
+#------------------------------------------------------------
+# Set up location specific data
+#-----------------------------------------------------------
+#if looking at ColRiver flows read in flow data from USGS gauge at Lees Ferry, AZ between 1985 to the end of the last water year
+#flow <- readNWISdv("09380000", "00060", "1985-10-01", "2021-09-30")
+#flow.magnitude <- TimestepDischarge(flow, 85000) #discharge data from USGS is in cfs, bankfull dischage (pers comm TK) 85000 cfs
+
+#if looking at ColRiver temps read in temperature data from USGS gauge at Lees Ferry, AZ between _ to the end of last water year
 temp <- readNWISdv("09380000", "00010", "2007-10-01", "2021-09-30")
+#temps <- TimestepTemperature(temp, "Colorado River") # can get time series data if desired
+#degreedays <- TimestepDegreeDay(temp, "Colorado River")
 
+#-------------------------------------------------------------
+# create a series of average 2 weekly data
+#-------------------------------------------------------------
+# calculate mean temperature data for each timestep (2 week interval)
+temps <- average.yearly.temp(temp, "X_00010_00003", "Date")
 
-flow.magnitude <- TimestepDischarge(flow, 85000) #discharge data from USGS is in cfs, bankfull dischage (pers comm TK) 85000 cfs
-temps <- TimestepTemperature(temp, "Colorado River") # calculate mean temperature data for each timestep
-degreedays <- TimestepDegreeDay(temp, "Colorado River")
+# if you want to augment ColRiver temps, set qr to different in C
+qr <- 0
+# if you want to augement how many years at temps, change r
+r <- 13 # I want 13 years
+temps <- rep.avg.year(temps, 13, change.in.temp = qr, years.at.temp = r)
 
+# calculate accumulated degreedays, which is the days above critical threhold * temperature (degC)
+# we assume accumulated degree days = 2-week average * 14
+degreedays <- as.data.frame(cbind(temps$dts, temps$Temperature * 14))
+colnames(degreedays) <- c("dts", "DegreeDay") # format
+degreedays$dts <- as.Date(degreedays$DegreeDay, origin = "1970-01-01") # make date-time
+
+# if desired, can pre-count how many degreedays each cohort should experience
+#forward.count.degreedays(559)
+
+#----------------------------------------------
+# set model parameters
+#----------------------------------------------
 # specify iterations
-iterations <- 5
-#species <- c("BAET", "SIMU", "CHIRO")
+iterations <- 50
 
 # baseline K in the absence of disturbance
 Kb <- 10000
@@ -49,25 +74,13 @@ Kb <- 10000
 Kd <- 40000
 
 # specify baseline transition probabilities for each species
-G1_BAET = 0.2
-G2_BAET = 0.2
-P1_BAET = 0.2
-P2_BAET = 0.2
+G1_BAET = 0.25 #move to Stage2 (subimago)
+G2_BAET = 0.25 #move to Stage3 (adult)
+P1_BAET = 0.3 #stay in Stage1 (larvae)
+P2_BAET = 0.3 #stay in Stage2 (subimago)
 
-
-# want to run this for one year, in 14 day timesteps 
-timestep <- seq(2, (length(temps$Temperature) + 1), by = 1) # OR
-#timestep <- seq(2, (length(out_sample) + 1), by = 1)
-
-# create an array to put our output into
-#output.N.array <- array(0, dim = c(length(timestep) + 1, length(species)))
-output.N.array <- array(0, dim = c(length(timestep) + 1))
-
-output.N.list <- list(output.N.array)
-
-
-## Assigning names to each array from sppnames vector
-#names(output.N.list) <- species
+# want this to run as long as our temperature timeseries
+timestep <- seq(2, (length(temps$Temperature) + 1), by = 1) 
 
 # create array to put the total N of all species into
 Total.N <- array(0,
@@ -87,17 +100,19 @@ output.N.list <- reparray
 #names(replist) <- species
 
 
-# Q is equal to average discharge over 14 days
-Q <- flow.magnitude$Discharge
+# Q is equal to average discharge over 14 days/bankful discharge for the system
+# in the Colorado River, bankful discharge = 85000 cfs (personal communication with Theodore Kennedy)
+#Q <- flow.magnitude$Discharge 
+
+# We want to create a model with no flow mortality, so set Q less than Qmin
 Q <- rep(0.1, length(temps$Temperature))
 
-
-Qmin <- 0.25
-a <- 0.1
-g <- 0.1
-h <- surv.fit.BAET$m$getPars()[2]   
-k <- surv.fit.BAET$m$getPars()[1]   
-extinction <- 500
+Qmin <- 0.25 # Q min is the minimum flow required to impact mortality and carryin capactity (K)
+a <- 0.1 #shape param for flow magnitude and K
+g <- 0.1 #shape param for relationship between K and time since disturbance
+h <- surv.fit.BAET$m$getPars()[2] # shape param for flood mortality
+k <- surv.fit.BAET$m$getPars()[1] # shape param for flood mortality  
+extinction <- 50 # extinction threshold - if Total abundance below this, population goes extinct
 
 #-------------------------
 # Outer Loop of Iterations
@@ -107,38 +122,32 @@ extinction <- 500
 for (iter in c(1:iterations)) {
   
   K = 10000 # need to reset K for each iteration
-  # we can also create a random flow scenario by sampleing flows
-  #out_sample <- sample(out$Discharge,length(out$Discharge), replace=TRUE)
-  #Q <- out_sample
   
-  # another option is to keep flow the same each timestep (to test temp effects)
-  #out_same <- rep(10000, length(out$Discharge))
-  #Q <- out_same
-  
-  
-  # need to assign starting value
-  # we can start with 10 S1 individuals for each species
-  #for (sp in species){
-  #  output.N.list[[sp]][1,1] <- 10
-  #}
-  
-  # or we can pull randomw values from a uniform distribution 
-  output.N.list[1,1:3, iter]<- runif(3, min = 1, max = (0.5*K))
+  # pull random values from a uniform distribution for starting pop
+  output.N.list[1,1:3, iter]<- runif(3, min = 1, max = (0.3*K))
   
   # we often want to look at different parameter values after we run code, so we create some lists
-  
   # list to input Ks
   Klist <- vector()
-  Klist[1] <- 10000
-  # list to imput flow morts
+  Klist[1] <- 10000 #set first K
+  
+  # list to input flow mortality
   flowmortlist <- vector()
   
+  # list to input fecundities
   Flist <- vector()
   
+  # list to input back-looking emergence times
   emergetime <- vector()
   
+  # list to input size
   sizelist <- vector()
   
+  # list to input probs to remaining in same stage
+  Glist <- vector()
+  
+  # list to input eigenvalue
+  eigenlist <- vector()
   #-------------------------
   # Inner Loop of Timesteps
   #-------------------------
@@ -148,35 +157,31 @@ for (iter in c(1:iterations)) {
     #----------------------------------------------------------
     # Calculate how many timesteps emerging adults have matured
     
-    emergetime <- append(emergetime, back.count.degreedays(t, 406)) # or 406? 266 degree days used from REF days above 10C. Add 266 + 10*14 
+    emergetime <- append(emergetime, back.count.degreedays(t, 559)) # value from Perry and Kennedy, 2016 
     #---------------------------------------------------------
     # Calculate fecundity per adult
     
     # we start by pulling fecundities from normal distribution
-    F_BAET = rnorm(1, mean = 1104.5, sd = 42.75)*0.5*0.25 #* H_BAET #Baetidae egg minima and maxima from Degrange, 1960 *0.5 assuming 50% female and 0.8 because 80% survival of eggs according to McMullen 2019 supplemental materials
+    F_BAET = rnorm(1, mean = 1104.5, sd = 42.75) * 0.5 * 0.5  #Baetidae egg minima and maxima from Degrange, 1960, assuming 1:1 sex ratio and 50% egg mortality
     
-    # relate fecundities to temperature based on Sweeney et al., 2017  *0.5 assuming 50% female and * 0.5 assuming 60% mort.
-    #F_BAET <- (-379.8021 * (temps$Temperature[t-1]) + 16.4664*(temps$Temperature[t-1]^2) - 0.2684* (temps$Temperature[t-1]^3) + 4196.8608) * 0.5 * 0.5
+    # we can also relate fecundities to body mass.
+    # Sweeney and Vannote 1980 have recorded dry body weight between 0.9 and 2.0 mg. 
+    # That weight is related to fecundity Y = 614X - 300
+    # we can "convert" emergetime to mg by multiplying to get dry weights between 0.9 - 2 mg, and then convert to fecunity
+    # Issue: this data is for Ephemerella spp, not Baetidae spp
     
-    # we can also relate fecundities to body mass. Sweeney and Vannote 1980 have recorded dry body weight between 0.9 and 2.0 mg. 
-    # that weight is related to fecundity Y = 614X - 300
-    # we can "convert" emergetime to mg by multiplying by 0.225 (to get dry weights between 0.9 - 2 mg)
-    
-    if (t > 5) {
+    if (t > 15) {
       size <- (emergetime[t-1] * 0.55)-0.75
       sizelist <- append(sizelist, size)
-      F_BAET <- ((614 * size) - 300)* 0.5*0.25 #* hydropeaking.mortality(lower = 0.0, upper = 0.2, h = hp[t-1])
+      F_BAET <- ((614 * size) - 300)* 0.5 * 0.5 
     }
     #--------------------------------------------------
-    # Calculate the disturbance magnitude-K relationship 
-    #Klist[1] <- 10000
-    
     # Calculate the disturbance magnitude-K relationship
     # Sets to 0 if below the Qmin
     Qf <- Qf.Function(Q[t-1], Qmin, a)
     
     #-------------------------------------------------------------------
-    # Calculate K arrying capacity immediately following the disturbance
+    # Calculate K carrying capacity immediately following the disturbance
     K0 <- K + ((Kd-K)*Qf)
     
     # Calculate final K for timestep, including relationship between K and time since disturbance
@@ -184,13 +189,29 @@ for (iter in c(1:iterations)) {
     
     Klist <- append(Klist, K)
     #---------------------------------------------
-    # Calculate effect of density dependnce on fecundity 
+    # Calculate effect of density dependence on fecundity 
     
     # Logistic via Rogosch et al. Fish Model
     # no immediate egg mortality incorporated
     F_BAET <- Logistic.Dens.Dependence(F_BAET, K, Total.N[t-1, iter])
     
+    # add F_BAET to list
     Flist <- append(Flist, F_BAET)
+    #-----------------------------------------------
+    # Calculate new transition probabilities based on temperature
+    # This is the growth v development tradeoff
+    
+    # development measures
+    # in this function, we assume that if below the min temp threshold (9) no maturation occurs (slow maturation, large growth)
+    # if above the max temp threshold (15), no one remains more than 1 timestep in each stage (fast maturation, small growth)
+    
+    # Probabilities of remaining in stages (when temps low, high prob of remaining)
+    P1_BAET <- growth.development.tradeoff(temps$Temperature[t-1],  9, 13, 0.43, 0.0)
+    P2_BAET <- growth.development.tradeoff(temps$Temperature[t-1], 9, 13, 0.43, 0)
+    
+    # growth (if below 10C, no growth can occur - everything basically freezes, if between 10 and 11, prob of remaining in same stage = 0.6395, if above 13, prob of transition to next stage is 0 )
+    G1_BAET <- 0.43 - P1_BAET
+    G2_BAET <- 0.43 - P2_BAET
     #-----------------------------------------------
     # Create Lefkovitch Matrix
     
@@ -199,19 +220,8 @@ for (iter in c(1:iterations)) {
     BAET3 <- c(0, G2_BAET, 0) 
     
     ABAET <- rbind( BAET1, BAET2, BAET3)
+    eigenlist <- append(eigenlist, eigen(ABAET)$values[1])
 
-    #-----------------------------------------------
-    # Calculate new transition probabilities based on temperature
-    # This is the growth v development tradeoff
-
-    # development measures (basically, if below 10C, no development, if between 10 and 12, follows a function, if above 12, prob of transition to next stage is 0.6395
-    ABAET[3, 2] <- growth.development.tradeoff(temps$Temperature[t-1],  10, 13, 0.01, 0.5)
-    ABAET[2,1] <- growth.development.tradeoff(temps$Temperature[t-1], 10, 13, 0.01, 0.5)
-
-    # growth (if below 10C, no growth can occur - everything basically freezes, if between 10 and 11, prob of remaining in same stage = 0.6395, if above 13, prob of transition to next stage is 0 )
-    ABAET[2,2] <- growth.development.tradeoff(temps$Temperature[t-1], 10, 13, 0.5, 0.01)
-    ABAET[1,1] <- growth.development.tradeoff(temps$Temperature[t-1], 10, 13, 0.5, 0.01)
-    
     #--------------------------------------
     # Calculate abundances for each stage
     
@@ -229,27 +239,31 @@ for (iter in c(1:iterations)) {
     output.N.list[t,3,iter] <- flood.mortality(output.N.list[t,3,iter], k, h, Q[t-1], Qmin)
     
     flowmortlist <- append(flowmortlist, flood.mortality(1, k, h, Q[t-1], Qmin))
-    #replist[[1]][,,1] <- output.N.list[[1]]
-    # check extinction threshold
+    
+    #------------------------------------------------------
+    # check extinction threshold and if below set to 0
     Total.N[,iter] <- apply(output.N.list[,,iter],1,sum)
     if (Total.N[t, iter] < extinction){
       output.N.list[t,,iter] <- 0
       Total.N[t, iter] <- 0}
+    
+    
   } #-------------------------
     # End Inner Loop  
     #------------------------- 
 } #----------------------
   # End Outer Loop
   #----------------------
-
 #------------------
 # Analyzing Results
 #-------------------
 # summarizing iterations
 
 ## turning replist into a df
-means.list.BAET <- mean.data.frame(output.N.list, burnin = 27)
+means.list.BAET <- mean.data.frame(output.N.list, burnin = 0)
 
+
+# note how boom and bust this model is - K is set to be 10,000 not 100,000
 abund.trends.BAET <- ggplot(data = means.list.BAET, aes(x = timesteps,
                                        y = mean.abund, group = 1)) +
   geom_ribbon(aes(ymin = mean.abund - 1.96 * se.abund,
@@ -260,70 +274,5 @@ abund.trends.BAET <- ggplot(data = means.list.BAET, aes(x = timesteps,
   geom_line(show.legend = FALSE) +
   coord_cartesian(ylim = c(0,100000)) +
   ylab('Baetis Abundance') +
-  xlab('Timestep')
+  xlab('Timestep')  
 
-saveRDS(abund.trends, paste0('BAETplot', '.rds'))
-
-
-
-# take a look at results
-# 
-# par(mfrow = c(1,1))
-plot(timestep[9:(length(timestep)+1)], output.N.list[9:(length(timestep)+1), 3, 1], type = "l", ylab = "Baetis spp. Adults", xlab = "Timestep (1 fortnight)")
-plot(timestep[9:length(timestep)], Total.N[10:(length(timestep)+1)], type= "l", ylab = "Baetis spp. Total N", xlab = "Timestep (1 fortnight)")
-# 
-# 
-# 
-# #creating plots to analyze how temp relationship is working
-# #create dataframe with timestemp, s1, s2, and s3 abundances, and tempterature
-#  data <- as.data.frame(cbind(timestep, output.N.list[2:(length(timestep)+1) ,1, 1], output.N.list[2:(length(timestep)+1) ,2, 1], output.N.list[2:(length(timestep)+1) ,3, 1], temps$Temperature))
-#  colnames(data) <- c("timestep", "Stage1", "Stage2", "Stage3", "Temperature")
-# 
-#  data <- data[10:60, ]
-# ggplot(data = data, aes(x = timestep, y = Stage1, color = "Stage1"))+
-#    geom_path()+
-#    geom_path(aes(x = timestep, y = Stage2, color = "Stage2"))+
-#    geom_path(aes(x = timestep, y = Stage3, color = "Stage3"))+
-#    geom_path(aes(x = timestep, y = Temperature*200, color = "Temperature"))+
-#    scale_y_continuous(
-# 
-#      # Features of the first axis
-#      name = "Abundance",
-# 
-#      # Add a second axis and specify its features
-#      sec.axis = sec_axis( ~.*0.005, name="Temperature C")
-#    )
-# 
-#  # plot to show relationship between temp and fecundity
-#  plot(temps$Temperature, Flist, ylab = "Fecundity per individual", xlab = "Temperature (C)", pch = 19)
-# 
-#  plot(timestep[10:60], output.N.list[10:60, 3,1] * 1, type = "l", ylim  = c(0, 500), ylab = " ")
-#  lines(timestep[10:60], ((Flist[10:60]*output.N.list[10:60, 3,1])/10), col = "blue", ylim = c(0, 450))
-#  lines(timestep[10:60], temps$Temperature[10:60]*15, col = "red")
-# lines(timestep[10:60], emergetime[4:54]*18, col = "green")
-#  lines(timestep[10:60], sizelist[4:54]*100, col = "magenta")
-#  legend(25, 430, legend = c("Adult Abundance", "Realized Fecundity per female * 0.1", "Temperature (C) * 15", "Dry Weight (mg) * 100"),
-#         col = c("black", "blue", "red", "magenta"), lty = 1, cex = 0.8)
-# 
-#  plot(timestep, temps$Temperature, type = "l", col = "blue")
-# 
-#  par(mfrow = c(1,2))
-#  plot(timestep[10:37], Glist[10:37], type = "l", col = "blue")
-#  lines(timestep[10:37], Plist[10:37], type = "l", col = "black")
-#  legend(18, 0.1, legend=c("Growth (remain)", "Development (transition)"),
-#         col=c("Black", "Blue"), lty=1, cex=0.8)
-#  plot(timestep[10:37], temps$Temperature[10:37], type = "l", col = "red")
-# 
-#  plot(timestep[200:210], Total.N[201:211], type= "l", ylab = "Baetis spp. Total N", xlab = "Timestep (1 fortnight")
-#  par(new=TRUE)
-#  lines(timestep[200:210],temps$Temperature[201:211],col="green")
-# 
-#  Total.N
-# 
-#  r <-Total.N[2:(length(timestep)+1)]/Total.N[1:length(timestep)]
-#  plot(timestep, r, type = "l")
-# 
-#  plot(Q, Klist[1:940])
-# 
-#  
-#  
