b85a7349bfb1c0715b9d97a62f5142745047efb1
Angelika Kurthen <kurthena@oregonstate.edu>
2023-02-10 14:18:50 -0800

    added code that calculates lengths based on growth rates for size-based

--- NZMS_shell_length_fecundity.R
+++ NZMS_shell_length_fecundity.R
@@ -23,3 +23,13 @@ summary(lm) # Fecundity = -50.2907 + (16.5408 * x)
 # predicted fecundity at 5.5 = 40.6837
 -50.2907 + (16.5408 * 5.5)
 # 
+
+
+# growth - according to _ growth is dependent on shell length, as is fecundity 
+# we want 1 class of non-reproductive subadults (smaller than 3.2 mm) and two classes of reproductive adults (3.2 mm and larger)
+# growth follows the equation growth per day = -0.006*length[t]+0.029 (Cross et al., 2010)
+# this can be re-written as length[t+1] = 0.994*length[t]+0.029
+shell.growth(0.994, 0.029, 0.5)
+# on day 164, length ~ 3.2 (maturity) - so on week 24, maturity reached [stage 1 = 0.5mm - 3.2 mm] in about 24 weeks or 12 timesteps
+# on day 266, length ~ 3.9538776 - [stage 2 = 3.2 - 3.9538776] in about 14 weeks or 7 timesteps
+# that means [stage 3 = 3.9538776+] for about 14 weeks or 7 timesteps
