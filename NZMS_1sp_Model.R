8d36263d0645d471e7b9d902cefc582a5fa0d4d7
Angelika Kurthen <kurthena@oregonstate.edu>
2023-02-19 15:22:01 -0800

    made it into a function

--- NZMS_1sp_Model.R
+++ NZMS_1sp_Model.R
@@ -24,95 +24,90 @@ library(dataRetrieval)
 # library(ggplot2, lib.loc = "/home/ib/kurthena/R_libs/4.2.1")
 # library(dataRetrieval, lib.loc = "/home/ib/kurthena/R_libs/4.2.1")
 
-# source functions
-source("NZMSSurvivorship.R")
-source("1spFunctions.R")
-source("ColoradoRiverTempRamp.R")
-# growth - according to _ growth is dependent on shell length, as is fecundity 
-# we want 1 class of non-reproductive subadults (smaller than 3.2 mm) and two classes of reproductive adults (3.2 mm and larger)
-# growth follows the equation growth per day = -0.006*length[t]+0.029 (Cross et al., 2010)
-# this can be re-written as length[t+1] = 0.994*length[t]+0.029
-shell.growth(0.994, 0.029, 0.5)
-# on day 164, length ~ 3.2 (maturity) - so on week 24, maturity reached [stage 1 = 0.5mm - 3.2 mm] in about 24 weeks or 12 timesteps
-# on day 266, length ~ 3.9538776 - [stage 2 = 3.2 - 3.9538776] in about 14 weeks or 7 timesteps
-# that means [stage 3 = 3.9538776+] for about 14 weeks or 7 timesteps
-
-
 #read in flow data from USGS gauge at Lees Ferry, AZ between 1985 to the end of the last water year
-flow <- readNWISdv("09380000", "00060", "1985-10-01", "2021-09-30")
-# read in temperature data from USGS gauge at Lees Ferry, AZ between _ to the end of last water year
 temp <- readNWISdv("09380000", "00010", "2007-10-01", "2021-09-30")
+temps <- average.yearly.temp(temp, "X_00010_00003", "Date")
 
-#flow.magnitude <- TimestepDischarge(flow, 85000) #discharge data from USGS is in cfs, bankfull dischage (pers comm TK) 85000 cfs
-temps <- TimestepTemperature(temp, "Colorado River") # calculate mean temperature data for each timestep
-degreedays <- TimestepDegreeDay(temp, "Colorado River")
+temp <- read.delim("gcmrc20230123125915.tsv", header=T)
+colnames(temp) <- c("Date", "Temperature")
+temps <- average.yearly.temp(temp, "Temperature", "Date")
 
-## Uncomment if Using Colorado River Temp Ramp 
-temps <- temp_seq
-degreedays <- temps$Temperature * 14
+n <- 13
+# qr is the temp ramps I want to increase the average Lees Ferry temp by 
+# how many years I want each temp ramp to last
+qr <- 0
+r <- 13
 
+temps <- rep.avg.year(temps, n, change.in.temp = qr, years.at.temp = r)
+discharge <- rep(0.1, times = length(temps$Temperature))
+source("1spFunctions.R")
 
-
-# specify iterations
-iterations <- 5
-
-# baseline K in the absence of disturbance
-Kb <- 10000
-# max K after a big disturbance
-Kd <- 40000
-
-# specify baseline transition probabilities
-# its speculated (Cross et al 2010) that survivorship is between 80 - 100% for NZMS in Grand Canyon - will say 90% survive, 10% baseline mortality
-# from timestep to timestep, we expect 90% to survive so for stage 1 (which lasts aproximately 14 timesteps, survival should be approx 09^14 = 0.2287679
-# from that, only 1/14th will transition out, 13/14 remain in stage
-
-# stage 1 G1 (prob transition to stage 2) = 0.95^14/14
-# stage 1 P1 (prob remaining in stage 2) = 0.95^14 -0.95^14/14
-# stage 2 G2 (prob transition to stage 3) = 0.95^7/7
-# stage 2 P2 (prob remaining in stage 2) = 0.95^7 - 0.95^7/7
-# stage 3 P3 (prob remaining in stage 3) = 0.95 ^5 - 0.95^5/5
-
-G1_NZMS = 0.1
-G2_NZMS = 0.1
-P1_NZMS = 0.4
-P2_NZMS = 0.5
-P3_NZMS = 0.8
-
-# want to run this for one year, in 14 day timesteps 
-#timestep <- seq(2, (length(flow.magnitude$Discharge) + 1), by = 1) # OR
-timestep <- seq(2, (length(temps$Temperature) + 1), by = 1)
-# create an array to put our output into
-output.N.array <- array(0, dim = c(length(timestep) + 1))
-
-output.N.list <- list(output.N.array)
-
-# create array to put the total N of all species into
-Total.N <- array(0,
-                 dim  <-c((length(timestep) +1 ), iterations),
-                 dimnames <- list(1:(length(timestep) + 1), 1:iterations))
-
-# create list of arrays w/ abundance data for each spp
-reparray <- array(0,
-                  
-                  dim = c(length(timestep) + 1, 3, iterations),
-                  dimnames = list(1:(length(timestep)+1), c("S1", "S2", "S3"), 1:iterations)
-)
-
-output.N.list <- reparray
-
-# Q is equal to average discharge over 14 days
-#Q <- flow.magnitude$Discharge
-
-Q <- rep(0.1, length(temps$Temperature))
-
-Qmin <- 0.25
-a <- 0.1
-g <- 0.1
-h <- surv.fit.NZMS$m$getPars()[2]   
-k <- surv.fit.NZMS$m$getPars()[1]
-e = 2.71828
-extinction <- 1
-
+NZMSmodel <- function(flow.data, temp.data, baselineK, disturbanceK, Qmin, extinct, iteration, peaklist = NULL, peakeach = NULL){
+  # source functions
+  source("NZMSSurvivorship.R")
+  Q <- as.numeric(flow.data)
+  temps <- temp.data
+  
+  degreedays <- as.data.frame(cbind(temps$dts, temps$Temperature * 14))
+  colnames(degreedays) <- c("dts", "DegreeDay")
+  degreedays$dts <- as.Date(degreedays$dts, origin = "1970-01-01")
+  
+  # need to make ramped increasing hydropeaking index 
+  hp <- c(rep(peaklist, each = peakeach))
+  
+  # specify iterations
+  iterations <- iteration
+  
+  # baseline K in the absence of disturbance
+  Kb <- as.numeric(baselineK)
+  # max K after a big disturbance
+  Kd <- as.numeric(disturbanceK)
+  
+  # specify baseline transition probabilities
+  # its speculated (Cross et al 2010) that survivorship is between 80 - 100% for NZMS in Grand Canyon - will say 90% survive, 10% baseline mortality
+  # from timestep to timestep, we expect 90% to survive so for stage 1 (which lasts aproximately 14 timesteps, survival should be approx 09^14 = 0.2287679
+  # from that, only 1/14th will transition out, 13/14 remain in stage (Caswell 2001, 6.4 p 160-161)
+  
+  # stage 1 G1 (prob transition to stage 2)
+  # stage 1 P1 (prob remaining in stage 2) 
+  # stage 2 P2 (prob remaining in stage 2)
+  # stage 3 P3 (prob remaining in stage 3) 
+  
+  G1 = 0.9 * 1/14
+  G2 = 0.9 * 1/7
+  P1 = 0.9 * 6/7
+  P2 = 0.9 * 13/14
+  P3 = 0.9 * 6/7 
+  
+  # want to run this for one year, in 14 day timesteps 
+  timestep <- seq(2, (length(temps$Temperature) + 1), by = 1)
+  
+  # create an array to put our output into
+  output.N.array <- array(0, dim = c(length(timestep) + 1))
+  
+  output.N.list <- list(output.N.array)
+  
+  # create array to put the total N of all species into
+  Total.N <- array(0,
+                   dim  <-c((length(timestep) +1 ), iterations),
+                   dimnames <- list(1:(length(timestep) + 1), 1:iterations))
+  
+  # create list of arrays w/ abundance data for each spp
+  reparray <- array(0,
+                    
+                    dim = c(length(timestep) + 1, 3, iterations),
+                    dimnames = list(1:(length(timestep)+1), c("S1", "S2", "S3"), 1:iterations)
+  )
+  
+  output.N.list <- reparray
+  
+  Qmin <- Qmin
+  a <- 0.1
+  g <- 0.1
+  h <- surv.fit.NZMS$m$getPars()[2]  
+  k <- surv.fit.NZMS$m$getPars()[1] 
+  
+  extinction <- extinct
 #-------------------------
 # Outer Loop of Iterations
 #--------------------------
@@ -120,24 +115,9 @@ extinction <- 1
 
 for (iter in c(1:iterations)) {
   
-  K = 10000 # need to reset K for each iteration
-  
-  # we can also create a random flow scenario by sampling flows
-  #out_sample <- sample(out$Discharge,length(out$Discharge), replace=TRUE)
-  #Q <- out_sample
-  
-  # another option is to keep flow the same each timestep (to test temp effects)
-  #out_same <- rep(10000, length(out$Discharge))
-  #Q <- out_same
-  
-  
-  # need to assign starting value
-  # we can start with 10 S1 individuals for each species
-  #for (sp in species){
-  #  output.N.list[[sp]][1,1] <- 10
-  #}
-  # or we can pull random values from a uniform distribution 
-  output.N.list[1,1:3, iter]<- runif(3, min = 1, max = (0.5*K))
+  K = Kb # need to reset K for each iteration
+  # we can pull random values from a uniform distribution 
+  output.N.list[1,1:3, iter]<- runif(3, min = 1, max = (0.3*K))
   
   # we often want to look at different parameter values after we run code, so we create some lists
   # list to input Ks
@@ -159,18 +139,17 @@ for (iter in c(1:iterations)) {
     #---------------------------------------------------------
     # Calculate starting fecundity per adult
     
-    # fecundities estimated from McKenzie et al. 2013; 
-    if (temps$Temperature[t-1] <= 10) { 
-      F2_NZMS <- 2
-      F3_NZMS <- 2  
+    # fecundities estimated from McKenzie et al. 2013 - reduced fecundity above 24 C and below 9 C. 
+    # optimal temp between 16 and 19 C, but we don't really have parameterization for that
+    if (temps$Temperature[t-1] <= 9 | temps$Temperature[t-1] >= 24) { 
+      F2 <- 2
+      F3 <- 2  
     } else {
-      F2_NZMS <- 8.87473
-      F3_NZMS <- 27.89665
+      F2 <- 8.87473
+      F3 <- 27.89665
     }
     
     #---------------------------------------------------
-    # Calculate the disturbance magnitude-K relationship 
-
     # Calculate the disturbance magnitude-K relationship
     # Sets to 0 if below the Qmin
     Qf <- Qf.Function(Q[t-1], Qmin, a)
@@ -180,25 +159,25 @@ for (iter in c(1:iterations)) {
     K0 <- K + ((Kd-K)*Qf)
     
     # Calculate final K for timestep, including relationship between K and time since disturbance
-    K <- post.dist.K(K0, Kb, g)
+    K <- post.dist.K(K0, Kb, g, t, Q, Qmin)
     Klist <- append(Klist, K)
     #---------------------------------------------
     # Calculate effect of density dependnce on fecundity 
     
-    # Logistic Density Dependence on Fecundity via Rogosch et al. Fish Model \
+    # Logistic Density Dependence on Fecundity via Rogosch et al. Fish Model
     # assume 97% Female poplation, and 30% instant mortality for eggs laid
-    F2_NZMS <- Logistic.Dens.Dependence(F2_NZMS, K, Total.N[t-1, iter]) * 0.97 * 0.7
-    F3_NZMS <- Logistic.Dens.Dependence(F3_NZMS, K, Total.N[t-1, iter]) * 0.97 * 0.7
+    F2 <- Logistic.Dens.Dependence(F2, K, Total.N[t-1, iter]) * 0.97 * 0.7
+    F3 <- Logistic.Dens.Dependence(F3, K, Total.N[t-1, iter]) * 0.97 * 0.7
     
-    Flist <- append(Flist, (F2_NZMS + F3_NZMS))
+    Flist <- append(Flist, (F2 + F3))
     #-----------------------------------------------
     # Create Lefkovitch Matrix
     
-    NZMS1 <- c(P1_NZMS, F2_NZMS, F3_NZMS)
-    NZMS2 <- c(G1_NZMS, P2_NZMS, 0)
-    NZMS3 <- c(0, G2_NZMS, P3_NZMS) 
+    S1 <- c(P1, F2, F3)
+    S2 <- c(G1, P2, 0)
+    S3 <- c(0, G2, P3) 
     
-    ANZMS <- rbind( NZMS1, NZMS2, NZMS3)
+    A <- rbind( S1, S2, S3)
     
     #-----------------------------------------------
     # Calculate new transition probabilities based on temperature
@@ -206,7 +185,7 @@ for (iter in c(1:iterations)) {
     #--------------------------------------
     # Calculate abundances for each stage
     
-    output.N.list[t, 1:3, iter] <- ANZMS %*% output.N.list[t-1, 1:3, iter] 
+    output.N.list[t, 1:3, iter] <- A %*% output.N.list[t-1, 1:3, iter] 
     
     #------------------------------------------
     #Calculate immediate mortality due to flows
@@ -234,14 +213,16 @@ for (iter in c(1:iterations)) {
 } #----------------------
 # End Outer Loop
 #----------------------
-
+}
+  return(output.N.list)
+}
 #------------------
 # Analyzing Results
 #-------------------
 # summarizing iterations
 
 ## turning replist into a df
-means.list.NZMS <- mean.data.frame(output.N.list,burnin = 25)
+means.list.NZMS <- mean.data.frame(output.N.list,burnin = 25, iteration= 5)
 means.list.NZMS <- cbind(means.list.NZMS[2:339,], temps$dts)
 means.list.NZMS$`temps$dts` <- as.Date(means.list.NZMS$`temps$dts`)
 # plot abundance over time
