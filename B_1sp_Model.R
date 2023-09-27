8a0a77fbfd7343558b8c455e6b16d754b32d40b2
Angelika Kurthen <kurthena@oregonstate.edu>
2023-09-27 12:54:15 -0700

    added updated P and G calculations and added temp survival

--- B_1sp_Model.R
+++ B_1sp_Model.R
@@ -42,7 +42,7 @@ Date <- rep(c(1:365), times = 5)
 Day <- seq(as.Date("2022-01-01"), as.Date("2026-12-31"), by="days")
 Day <- Day[-which(Day == "2024-02-29")]
 
-Temperature <-  -7.374528  * (cos(((2*pi)/365)*Date))  +  (-1.649263* sin(2*pi/(365)*Date)) + 10.956243
+Temperature <-  -7.374528  * (cos(((2*pi)/365)*Date))  +  (-1.649263* sin(2*pi/(365)*Date))  + 10.956243
 
 temp <- as.data.frame(cbind(Time, Day, Temperature))
 temp$Day <- as.Date(temp$Day, origin= "1970-01-01")
@@ -57,18 +57,21 @@ temp <- temp[c(1,3)]
 # extinct = 50
 # flow.data <- discharge
 # temp.data <- temp
-
+discharge <- rep(0.1, times = length(temp$Temperature))
+discharge[floor(runif(1, 90, 131))] <- runif(1, 0.25, 1)
 
 Bmodel <- function(flow.data, temp.data, baselineK, disturbanceK, Qmin, extinct, iteration, peaklist = NULL, peakeach = NULL, fecundity = 900, dds = 500){
   
 # set up model
 source("NegExpSurv.R")
-
+source("BAETSurvivorship.R")
+  
 Q <- as.numeric(flow.data)
 temps <- temp.data
   
 degreedays <- as.data.frame(cbind(temps$dts, temps$Temperature * 14))
 colnames(degreedays) <- c("dts", "DegreeDay")
+degreedays$DegreeDay[degreedays$DegreeDay<0] <- 0
 degreedays$dts <- as.Date(as.POSIXct(degreedays$dts, origin = "1970-01-01"))
   
 # need to make ramped increasing hydropeaking index 
@@ -154,7 +157,13 @@ for (iter in c(1:iterations)) {
   
   sizelist <- vector()
   delta <- vector()
-  development <- vector()
+
+  TempSurvival <- vector()
+
+  for(c in temps$Temperature){
+    b <- TempSurv(c)
+    TempSurvival <- append(TempSurvival, b)
+  }
   #-------------------------
   # Inner Loop of Timesteps
   #-------------------------
@@ -172,7 +181,7 @@ for (iter in c(1:iterations)) {
     
     # we start by pulling fecundities from normal distribution
     # assuming 50 50 sex ration, 0.22 of egg masses 'dissapearred', and 0.2 desiccation because of rock drying
-    F3 = fecundity * 0.5 * hydropeaking.mortality(0.0, 0.2, h = hp[t-1])
+    F3 = fecundity  * hydropeaking.mortality(0.0, 0.2, h = hp[t-1])
     #F3 = rnorm(1, mean = 1104.5, sd = 42.75) * 0.5  #Baetidae egg minima and maxima from Degrange, 1960, assuming 1:1 sex ratio and 50% egg mortality
     
     # we can also relate fecundities to body mass.
@@ -186,7 +195,7 @@ for (iter in c(1:iterations)) {
     if (t > 19) { # will be erased in burn
       size <- emergetime[t-1]
       sizelist <- append(sizelist, size)
-      F3 <- ((size*mod$coefficients[2])+mod$coefficients[1])* 0.5 * hydropeaking.mortality(0.0, 0.2, h = hp[t-1])
+      F3 <- ((size*mod$coefficients[2])+mod$coefficients[1]) * hydropeaking.mortality(0.0, 0.2, h = hp[t-1])
       #F3 <- (57*size)+506 * 0.5 * hydropeaking.mortality(0.0, 0.2, h = hp[t-1]) * 0.78 * 0.65
     }
     # size <- delta[t-1]
@@ -226,25 +235,25 @@ for (iter in c(1:iterations)) {
     # # Probabilities of remaining in stages (when temps low, high prob of remaining)
     #development measures (basically, if below 10C, no development, if between 10 and 12, follows a function, if above 12, prob of transition to next stage is 0.6395)
     if (5 > temps$Temperature[t-1]) {
-      P1 <- 1-(1/((delta[t-1]-1)/2))
-      P2 <- 1-(1/((delta[t-1]-1)/2))
-      G1 <- 0.1/((delta[t-1]-1)/2)
-      G2 <- 0.3/((delta[t-1]-1)/2)
+      P1 <- 0.2*(1-(1/((delta[t-1])/2)))
+      P2 <- 0.5*(1-(1/((delta[t-1])/2)))
+      G1 <- 0.2/((delta[t-1])/2)
+      G2 <- 0.5/((delta[t-1])/2)
       }
 
     if (temps$Temperature[t-1] > 21){
-      P1 <- 1-(1/((delta[t-1])/2))
-      P2 <- 1-(1/((delta[t-1])/2))
-      G1 <- 0.1/((delta[t-1])/2)
-      G2 <- 0.3/((delta[t-1])/2)
+      P1 <- 0.2*(1-(1/((delta[t-1])/2)))
+      P2 <- 0.5*(1-(1/((delta[t-1])/2)))
+      G1 <- 0.2/((delta[t-1])/2)
+      G2 <- 0.5/((delta[t-1])/2)
       }
 
     
     if (5 <= temps$Temperature[t-1] & temps$Temperature[t-1] <= 21 ){
-      G1 <- 0.1/((delta[t-1]-1)/2)
-      G2 <- 0.3/((delta[t-1]-1)/2)
-      P1 <- 1-(1/((delta[t-1]-1)/2))
-      P2 <- 1-(1/((delta[t-1]-1)/2))
+      G1 <- 0.2/((delta[t-1])/2)
+      G2 <- 0.5/((delta[t-1])/2)
+      P1 <- 0.2*(1-(1/((delta[t-1])/2)))
+      P2 <- 0.5*(1-(1/((delta[t-1])/2)))
     }
     # if (5 <= temps$Temperature[t-1] & temps$Temperature[t-1] <= 21 & is.na(emergetime[t] == T)) {
     #   G1 <- 0.1/((-0.353 * temps$Temperature[t-1]) + 10.059)
@@ -277,23 +286,23 @@ for (iter in c(1:iterations)) {
     #------------------------------------------
     
     # Calculate immediate mortality due to temperature regime (outside of thermal optima)
-    output.N.list[t, 1:3, iter] <- output.N.list[t, 1:3, iter]*TempSurv(temps$temperature[t])
-    
+    output.N.list[t, 1, iter] <- output.N.list[t, 1, iter]*TempSurvival[t-1]
+    output.N.list[t, 2, iter] <- output.N.list[t, 2, iter]*TempSurvival[t-1]
     #Calculate immediate mortality due to flows
     # mortality due to flooding follows N0 = Nz*e^-hQ
     
     #s1
-    output.N.list[t-1, 1, iter] <- flood.mortality(output.N.list[t-1, 1, iter], k, h, Q[t-1], Qmin)
+    output.N.list[t, 1, iter] <- flood.mortality(output.N.list[t, 1, iter], k, h, Q[t-1], Qmin)
     #s2Q
-    output.N.list[t-1,2,iter] <- flood.mortality(output.N.list[t-1,2,iter], k, h, Q[t-1], Qmin)
+    output.N.list[t,2,iter] <- flood.mortality(output.N.list[t,2,iter], k, h, Q[t-1], Qmin)
     
-    output.N.list[t-1,3,iter] <- flood.mortality(output.N.list[t-1,3,iter], k, h, Q[t-1], Qmin)
+    output.N.list[t,3,iter] <- flood.mortality(output.N.list[t,3,iter], k, h, Q[t-1], Qmin)
     
     flowmortlist <- append(flowmortlist, flood.mortality(1, k, h, Q[t-1], Qmin))
     
     #------------------------------------------------------
     # check extinction threshold and if below set to 0
-    Total.N[,iter] <- apply(output.N.list[,,iter],1,sum)
+    Total.N[t,iter] <- sum(output.N.list[t,,iter])
     if (Total.N[t, iter] < extinction){
       output.N.list[t,,iter] <- 0
       Total.N[t, iter] <- 0}
@@ -313,45 +322,52 @@ return(output.N.list[ ,1:3, ])
 #-------------------
 # summarizing iterations
 
-# out <- BAETmodel(flow.data = discharge, temp.data = temps, disturbanceK = 40000, baselineK = 10000, Qmin = 0.25, extinct = 500, iteration = 1, peaklist = 0, peakeach = length(temps$Temperature))
+ out <- Bmodel(flow.data = discharge, temp.data = temp, disturbanceK = 40000, baselineK = 10000, Qmin = 0.25, extinct = 50, iteration = 3, peaklist = 0, peakeach = length(temp$Temperature))
 # 
-# adults <-as.data.frame(cbind(temps$dts, out[1:length(temps$dts),3,1]))
-# colnames(adults) <- c("Time","Adult Baetidae")
+adults <-as.data.frame(cbind(temp$dts, out[1:length(temp$dts),3,1]))
+colnames(adults) <- c("Time","Adult Baetidae")
+adults$Time <- as.Date(as.POSIXct(adults$Time, origin = "1970-01-01"))
+
 # 
 # ## turning replist into a df
-# means.list.BAET <- mean.data.frame(out, burnin = 0, iteration = 1)
-# means.list.BAET <- cbind(means.list.BAET[1:length(means.list.BAET$mean.abund),], temps$dts[1:length(means.list.BAET$mean.abund)])
-# means.list.BAET$`temps$dts` <- as.Date(means.list.BAET$`temps$dts`)
+ means.list.BAET <- mean.data.frame(out, burnin = 90, iteration = 3)
+ means.list.BAET <- cbind(means.list.BAET[1:length(means.list.BAET$mean.abund),], temp$dts[90:132])
+ means.list.BAET$`temp$dts` <- as.Date(means.list.BAET$`temp$dts`)
 # 
 # # note how boom and bust this model is - K is set to be 10,000 not 100,000
-# abund.trends.BAET <- ggplot(data = adults[300:500,], aes(x = Time,
-#                                        y = `Adult Baetidae`/10000, group = 1)) +
-#   geom_point()+
-#   # geom_ribbon(aes(ymin = mean.abund - 1.96 * se.abund,
-#   #                 ymax = mean.abund + 1.96 * se.abund),
-#   #             colour = 'transparent',
-#   #            alpha = .5,
-#   #             show.legend = FALSE) +
-#   geom_line(show.legend = FALSE) +
-#   coord_cartesian(ylim = c(0,2)) +
-#   ylab('Adult Baetis Abundance/ Baseline Reproductive Limit') +
-#   xlab('Timestep')  
+abund.trends.BAET <- ggplot(data = adults[90:131,], aes(x = Time,
+                                       y = `Adult Baetidae`/10000, group = 1)) +
+  geom_point()+
+  # geom_ribbon(aes(ymin = mean.abund - 1.96 * se.abund,
+  #                 ymax = mean.abund + 1.96 * se.abund),
+  #             colour = 'transparent',
+  #            alpha = .5,
+  #             show.legend = FALSE) +
+  geom_line(show.legend = FALSE) +
+  coord_cartesian(ylim = c(0,0.4)) +
+  ylab('Adult Mayfly Sp Abundance/ Baseline Reproductive Limit') +
+  xlab('Timestep')
 # 
 # plot(adults$`Adult Baetidae`[300:500], adults$`Adult Baetidae`[301:501], type = "b", xlab = "Adult Baetids t", ylab = "Adult Baetids t+1")
 # 
-# ggplot(data = means.list.BAET[300:500,], aes(x = `temps$dts`,
-#                           y = mean.abund/10000, group = 1)) +
-#   geom_point()+
-#   # geom_ribbon(aes(ymin = mean.abund - 1.96 * se.abund,
-#   #                 ymax = mean.abund + 1.96 * se.abund),
-#   #             colour = 'transparent',
-#   #            alpha = .5,
-#   #             show.legend = FALSE) +
-#   geom_line(show.legend = FALSE) +
-#   coord_cartesian(ylim = c(0,30)) +
-#   ylab('Baetis spp. Abundance/Reproductive Limit') +
-#   xlab('Timestep')  
-# 
+ggplot(data = means.list.BAET, aes(x = `temp$dts`,
+                          y = mean.abund/10000, group = 1)) +
+  geom_point()+
+  # geom_ribbon(aes(ymin = mean.abund - 1.96 * se.abund,
+  #                 ymax = mean.abund + 1.96 * se.abund),
+  #             colour = 'transparent',
+  #            alpha = .5,
+  #             show.legend = FALSE) +
+  geom_line(show.legend = FALSE) +
+  coord_cartesian(ylim = c(0,20)) +
+  ylab('Mayfly spp. Abundance/Reproductive Limit') +
+  xlab('Timestep')+
+  geom_line(aes(y = discharge[89:131]*12), color = "blue")+
+  scale_x_date(date_labels="%B", date_breaks  ="4 months")+
+  theme(text = element_text(size = 14), axis.text.x = element_text(angle=45, hjust = 1, size = 12.5), 
+        axis.text.y = element_text(size = 13), legend.key = element_rect(fill = "transparent"))
+
+
 # plot(means.list.BAET$mean.abund[300:500], means.list.BAET$mean.abund[301:501], type = "b", xlab = "Nt", ylab = "Nt+1")
 # 
 # ggplot(data = NULL, mapping = aes(x = temps$dts, y = Total.N[2:2003]/10000))+
