a5562de09ffd22e14cfe826890c93eb71282f5a3
Angelika Kurthen <kurthena@oregonstate.edu>
2024-01-20 10:23:57 -0800

    code to shift temperature regime up and down for life history sp A B C D

---
+++ CspTempToggle.R
@@ -0,0 +1,56 @@
+#############################
+# C sp temperature toggle
+############################
+
+
+#Code for HPC
+#library(lubridate, lib.loc = "/home/ib/kurthena/R_libs/4.2.1")
+
+library(lubridate)
+
+
+source("C_1sp_Model.R")
+source("1spFunctions.R")
+source("NegExpSurv.R")
+Time <- c(1:36500)
+Date <- rep(1:365, times = 100)
+Day <- seq(as.Date("2022-01-01"), as.Date("2121-12-31"), by="days")
+# find and remove leap days
+find_leap = function(x){
+  day(x) == 29 & month(x) == 2 
+}
+Day <- Day[which(find_leap(Day) == F)]
+
+
+Temperature <-  -7.374528  * (cos(((2*pi)/365)*Date))  +  (-1.649263* sin(2*pi/(365)*Date)) + 13.956243
+
+temp <- as.data.frame(cbind(Time, Day, Temperature))
+temp$Day <- as.Date(temp$Day, origin= "1970-01-01")
+colnames(temp) <- c("Time", "Date", "Temperature")
+temp <- TimestepTemperature(temp)
+temp <- temp[c(1,3)]
+
+Year <- year(temp$dts)
+uYear <- unique(Year)
+Month <- month(temp$dts)
+discharge <- rep(0.1, time = length(temp$dts))
+
+temp_means <- vector()
+temp_seq <- seq(-10, 10, by = 1)
+for (te in 1:length(temp_seq)){
+  print(te)
+  temp$Temperature <- temp$Temperature + temp_seq[te]
+  out <- Cmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
+  temp$Temperature <- temp$Temperature - temp_seq[te]
+  means.list.C <- mean.data.frame(out, burnin = 250, iteration = 2) 
+  temp_means[te] <- mean(means.list.C$mean.abund)
+  
+}
+
+temp_adjust_df <- as.data.frame(cbind(temp_seq, temp_means))
+ctemp <- ggplot(data = temp_adjust_df, mapping = aes(x = temp_seq, y = temp_means/10000))+
+  geom_line(size = 1, col = "#EE6677")+
+xlab("Degree C Change")+
+  ylab("C sp Abundance Relative to K")+
+  theme_bw()
+
