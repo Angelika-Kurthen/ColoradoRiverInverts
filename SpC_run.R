dfc38e919427c2e76758192dad0a30a2f6542482
Angelika Kurthen <kurthena@oregonstate.edu>
2024-01-25 09:41:40 -0800

    changed temp profile

--- SpC_run.R
+++ SpC_run.R
@@ -45,7 +45,7 @@ find_leap = function(x){
 Day <- Day[which(find_leap(Day) == F)]
 
 
-Temperature <-  -7.374528  * (cos(((2*pi)/365)*Date))  +  (-1.649263* sin(2*pi/(365)*Date)) + 10.956243
+Temperature <-  -7.374528  * (cos(((2*pi)/365)*Date))  +  (-1.649263* sin(2*pi/(365)*Date)) + 13.956243
 
 temp <- as.data.frame(cbind(Time, Day, Temperature))
 temp$Day <- as.Date(temp$Day, origin= "1970-01-01")
@@ -158,8 +158,12 @@ JanDist <- ggplot(data = m, aes(x = `temp$dts[249:2608]`, y = mean.abund/10000, 
 all.dates <- unique(format(temp$dts, "%m-%d"))[order(unique(format(temp$dts, "%m-%d")))]
 # loop to select a date from a Week-Month combo from each unique year
 means <- list()
-jday_data <- data.frame(date = NULL, post_dist = NULL, abund = NULL)
+#jday_data <- data.frame(date = NULL, post_dist = NULL, abund = NULL)
 jday_max <- vector()
+
+short_mean <- vector()
+resil_mean <- vector()
+long_mean <- vector()
 
 for (d in 1:length(all.dates)){ # 30 reps takes 60 mins
   sample_dates <- temp$dts[which(format(temp$dts, "%m-%d")== all.dates[d])]
@@ -175,12 +179,36 @@ for (d in 1:length(all.dates)){ # 30 reps takes 60 mins
   m <- mean.data.frame(out, burnin = 250, iteration = 9)
   m <- cbind(m, discharge[250:402])
   means[d] <- list(m)
-  abund <- means[[d]][(which(discharge == "0.3") - 248):(which(discharge == "0.3") - 238),2 ]
-  post <- seq(0, 10, by = 1)
-  dat <- rep(d, times = length(post))
-  jday_data <- as.data.frame(rbind(jday_data, cbind(dat, post, abund)))
+  short_abund <- means[[d]][which(discharge[250:402] == "0.3"), 2]
+  resil_abund <- means[[d]][(which(discharge[250:402] == "0.3") + 1):(which(discharge[250:402] == "0.3") + 6), 2]
+  long_abund <- means[[d]][(which(discharge[250:402] == "0.3") + 26):(which(discharge[250:402] == "0.3") + 36), 2]
+  #post <- seq(0, 10, by = 1)
+  #dat <- rep(d, times = length(post))
+  #jday_data <- as.data.frame(rbind(jday_data, cbind(dat, post, abund)))
+  short_mean[d] <- short_abund
+  resil_mean[d] <- mean(resil_abund)
+  long_mean[d] <- mean(long_abund)
 }
 
+short_df <- as.data.frame(cbind(short_mean, all.dates))
+resil_df <- as.data.frame(cbind(resil_mean, all.dates))
+long_df <- as.data.frame(cbind(long_mean, all.dates))
+
+short_df$short_mean <- as.numeric(short_df$short_mean)
+resil_df$resil_mean <- as.numeric(resil_df$resil_mean)
+long_df$long_mean <- as.numeric(long_df$long_mean)
+
+cshort <- ggplot(data = short_df, aes(all.dates, short_mean, group = 1))+
+  geom_line()
+
+cresil <- ggplot(data = resil_df, aes(all.dates, resil_mean, group =1))+
+  geom_line()
+
+clong <- ggplot(data = long_df, aes(all.dates, long_mean, group = 1))+
+  geom_line()
+
+
+length(rollmean(short_df$short_mean, 3))
 jday.df <- as.data.frame(cbind(jday_max, all.dates))
 jday.df$jday_max <- as.numeric(jday.df$jday_max)
 jday.df$all.dates <- yday(as.POSIXct(jday.df$all.dates, format = "%m-%d"))
