bea80cbfc6c19e6f915352d07074d05516eb404b
Angelika Kurthen <kurthena@oregonstate.edu>
2024-03-11 11:46:23 -0700

    commented out a section and changed disturbance K

--- Annual.R
+++ Annual.R
@@ -129,75 +129,75 @@ pulse <- subset(pulse, Date >= "2035-01-01" & Date <= "2035-12-31")
 # annual <- subset(annual, Date >= "2035-01-01" & Date <= "2035-12-31")
 # annual <- subset(annual, Date >= "2035-01-01" & Date <= "2035-12-31")
 # 
-discharge <- rep(0.1, time = length(temp$dts))
-
-A_out <- Amodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
-A_annual <- cbind.data.frame(temp$dts[250:length(A_out)], A_out[250:length(A_out)], rep("A", length(A_out)-249))
-B_out <- Bmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
-B_annual <- cbind.data.frame(temp$dts[250:length(B_out)], B_out[250:length(B_out)], rep("B", length(B_out)-249))
-C_out <- Cmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
-C_annual <- cbind.data.frame(temp$dts[250:length(C_out)], C_out[250:length(C_out)], rep("C", length(C_out)-249))
-D_out <- Dmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
-D_annual <- cbind.data.frame(temp$dts[250:length(D_out)], D_out[250:length(D_out)], rep("D", length(D_out)-249))
-
-
-colnames(A_annual) <- c("Date", "Adults", "Taxa")
-colnames(B_annual) <- c("Date", "Adults", "Taxa")
-colnames(C_annual) <- c("Date", "Adults", "Taxa")
-colnames(D_annual) <- c("Date", "Adults", "Taxa")
-
-
-annual <- rbind(A_annual, B_annual, C_annual, D_annual)
-annual$Date <- as.Date(annual$Date)
-annual <- subset(annual, Date >= "2035-01-01" & Date <= "2035-12-31")
-ggplot(data = annual, aes(x = Date, y  =Adults, color = Taxa))+
-  geom_point(size = 1, alpha = 0.5)+
-  geom_line(size = 1)+
-  ylim(c(0,20000))+
-  #stat_smooth(size= 1, span = 0.4, se =F)+
-  xlab("Month")+
-  ylab("Relativized Abundance")+
-  scale_color_manual(name = "Taxa", values=c("#66CCEE", "#228833", "#EE6677", "#AA3377"))+
-  scale_x_date(date_labels="%B", date_breaks  ="1 month")+
-  theme_bw()+
-  theme(text = element_text(size = 14), axis.text.x = element_text(hjust = 1, angle = 45, size = 12.5), 
-        axis.text.y = element_text(size = 13), legend.key = element_rect(fill = "transparent"))
-
-
-
-selected_date <- temp$dts[temp$dts >= as.Date("2035-05-01") & temp$dts <= as.Date("2035-05-15")]
-discharge[match(selected_date, temp$dts)] <- 0.3
-
-A_out <- Amodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
-A_annual <- cbind.data.frame(temp$dts[250:length(A_out)], A_out[250:length(A_out)], rep("A", length(A_out)-249))
-B_out <- Bmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
-B_annual <- cbind.data.frame(temp$dts[250:length(B_out)], B_out[250:length(B_out)], rep("B", length(B_out)-249))
-C_out <- Cmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
-C_annual <- cbind.data.frame(temp$dts[250:length(C_out)], C_out[250:length(C_out)], rep("C", length(C_out)-249))
-D_out <- Dmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
-D_annual <- cbind.data.frame(temp$dts[250:length(D_out)], D_out[250:length(D_out)], rep("D", length(D_out)-249))
-colnames(A_annual) <- c("Date", "Adults", "Taxa")
-colnames(B_annual) <- c("Date", "Adults", "Taxa")
-colnames(C_annual) <- c("Date", "Adults", "Taxa")
-colnames(D_annual) <- c("Date", "Adults", "Taxa")
-pulse <- rbind(A_annual, B_annual, C_annual, D_annual)
-pulse$Date <- as.Date(pulse$Date)
-pulse <- subset(pulse, Date >= "2035-01-01" & Date <= "2035-12-31")
-
-
-ggplot(data = pulse, aes(x = Date, y  =Adults, color = Taxa))+
-  geom_point(size = 1, alpha = 0.5)+
-  geom_line(size = 1)+
-  ylim(c(0,20000))+
-  #stat_smooth(size= 1, span = 0.4, se =F)+
-  xlab("Month")+
-  ylab("Relativized Abundance")+
-  geom_vline(xintercept = as.numeric(as.Date("2035-05-08")),
-             color = "black",
-             lwd = 1,
-             linetype = "dotted") +
-  scale_color_manual(name = "Taxa", values=c("#66CCEE", "#228833", "#EE6677", "#AA3377"))+
-  scale_x_date(date_labels="%B", date_breaks  ="1 month")+
-  theme_bw()+
-  theme(text = element_text(size = 14), axis.text.x = element_text(hjust = 1, angle = 45, size = 12.5),
-        axis.text.y = element_text(size = 13), legend.key = element_rect(fill = "transparent"))
+# discharge <- rep(0.1, time = length(temp$dts))
+# 
+# A_out <- Amodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
+# A_annual <- cbind.data.frame(temp$dts[250:length(A_out)], A_out[250:length(A_out)], rep("A", length(A_out)-249))
+# B_out <- Bmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
+# B_annual <- cbind.data.frame(temp$dts[250:length(B_out)], B_out[250:length(B_out)], rep("B", length(B_out)-249))
+# C_out <- Cmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
+# C_annual <- cbind.data.frame(temp$dts[250:length(C_out)], C_out[250:length(C_out)], rep("C", length(C_out)-249))
+# D_out <- Dmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
+# D_annual <- cbind.data.frame(temp$dts[250:length(D_out)], D_out[250:length(D_out)], rep("D", length(D_out)-249))
+# 
+# 
+# colnames(A_annual) <- c("Date", "Adults", "Taxa")
+# colnames(B_annual) <- c("Date", "Adults", "Taxa")
+# colnames(C_annual) <- c("Date", "Adults", "Taxa")
+# colnames(D_annual) <- c("Date", "Adults", "Taxa")
+# 
+# 
+# annual <- rbind(A_annual, B_annual, C_annual, D_annual)
+# annual$Date <- as.Date(annual$Date)
+# annual <- subset(annual, Date >= "2035-01-01" & Date <= "2035-12-31")
+# ggplot(data = annual, aes(x = Date, y  =Adults, color = Taxa))+
+#   geom_point(size = 1, alpha = 0.5)+
+#   geom_line(size = 1)+
+#   ylim(c(0,20000))+
+#   #stat_smooth(size= 1, span = 0.4, se =F)+
+#   xlab("Month")+
+#   ylab("Relativized Abundance")+
+#   scale_color_manual(name = "Taxa", values=c("#66CCEE", "#228833", "#EE6677", "#AA3377"))+
+#   scale_x_date(date_labels="%B", date_breaks  ="1 month")+
+#   theme_bw()+
+#   theme(text = element_text(size = 14), axis.text.x = element_text(hjust = 1, angle = 45, size = 12.5), 
+#         axis.text.y = element_text(size = 13), legend.key = element_rect(fill = "transparent"))
+# 
+# 
+# 
+# selected_date <- temp$dts[temp$dts >= as.Date("2035-05-01") & temp$dts <= as.Date("2035-05-15")]
+# discharge[match(selected_date, temp$dts)] <- 0.3
+# 
+# A_out <- Amodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
+# A_annual <- cbind.data.frame(temp$dts[250:length(A_out)], A_out[250:length(A_out)], rep("A", length(A_out)-249))
+# B_out <- Bmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
+# B_annual <- cbind.data.frame(temp$dts[250:length(B_out)], B_out[250:length(B_out)], rep("B", length(B_out)-249))
+# C_out <- Cmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
+# C_annual <- cbind.data.frame(temp$dts[250:length(C_out)], C_out[250:length(C_out)], rep("C", length(C_out)-249))
+# D_out <- Dmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 1, peaklist = 0, peakeach = length(temp$Temperature))
+# D_annual <- cbind.data.frame(temp$dts[250:length(D_out)], D_out[250:length(D_out)], rep("D", length(D_out)-249))
+# colnames(A_annual) <- c("Date", "Adults", "Taxa")
+# colnames(B_annual) <- c("Date", "Adults", "Taxa")
+# colnames(C_annual) <- c("Date", "Adults", "Taxa")
+# colnames(D_annual) <- c("Date", "Adults", "Taxa")
+# pulse <- rbind(A_annual, B_annual, C_annual, D_annual)
+# pulse$Date <- as.Date(pulse$Date)
+# pulse <- subset(pulse, Date >= "2035-01-01" & Date <= "2035-12-31")
+# 
+# 
+# ggplot(data = pulse, aes(x = Date, y  =Adults, color = Taxa))+
+#   geom_point(size = 1, alpha = 0.5)+
+#   geom_line(size = 1)+
+#   ylim(c(0,20000))+
+#   #stat_smooth(size= 1, span = 0.4, se =F)+
+#   xlab("Month")+
+#   ylab("Relativized Abundance")+
+#   geom_vline(xintercept = as.numeric(as.Date("2035-05-08")),
+#              color = "black",
+#              lwd = 1,
+#              linetype = "dotted") +
+#   scale_color_manual(name = "Taxa", values=c("#66CCEE", "#228833", "#EE6677", "#AA3377"))+
+#   scale_x_date(date_labels="%B", date_breaks  ="1 month")+
+#   theme_bw()+
+#   theme(text = element_text(size = 14), axis.text.x = element_text(hjust = 1, angle = 45, size = 12.5),
+#         axis.text.y = element_text(size = 13), legend.key = element_rect(fill = "transparent"))
