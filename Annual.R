8a776d24a0d4527487bab64d7493c494e58063cf
Angelika Kurthen <kurthena@oregonstate.edu>
2024-03-29 09:07:20 -0700

    upped disturbance K to 100000

--- Annual.R
+++ Annual.R
@@ -28,16 +28,16 @@ temp <- temp[c(1,3)]
 
 discharge <- rep(0.1, time = length(temp$dts))
 
-A_out <- Amodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
+A_out <- Amodel(discharge, temp, baselineK = 10000, disturbanceK = 1000000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
 A_out <- mean.data.frame(A_out, burnin = 250, iteration = 2)
 A_annual <- cbind.data.frame(temp$dts[250:last(A_out$timesteps)], A_out, rep("A", length(A_out$mean.abund)))
-B_out <- Bmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
+B_out <- Bmodel(discharge, temp, baselineK = 10000, disturbanceK = 1000000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
 B_out <- mean.data.frame(B_out, burnin = 250, iteration = 2)
 B_annual <- cbind.data.frame(temp$dts[250:last(B_out$timesteps)], B_out, rep("B", length(B_out$mean.abund)))
-C_out <- Cmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
+C_out <- Cmodel(discharge, temp, baselineK = 10000, disturbanceK = 1000000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
 C_out <- mean.data.frame(C_out, burnin = 250, iteration = 2)
 C_annual <- cbind.data.frame(temp$dts[250:last(C_out$timesteps)], C_out, rep("C", length(C_out$mean.abund)))
-D_out <- Dmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
+D_out <- Dmodel(discharge, temp, baselineK = 10000, disturbanceK = 1000000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
 D_out <- mean.data.frame(D_out, burnin = 250, iteration = 2)
 D_annual <- cbind.data.frame(temp$dts[250:last(C_out$timesteps)], D_out, rep("D", length(B_out$mean.abund)))
 
@@ -68,16 +68,16 @@ ggplot(data = annual, aes(x = Date, y  =Abundance/10000, color = Taxa))+
 selected_date <- temp$dts[temp$dts >= as.Date("2035-05-01") & temp$dts <= as.Date("2035-05-15")]
 discharge[match(selected_date, temp$dts)] <- 0.3
 
-A_pout <- Amodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
+A_pout <- Amodel(discharge, temp, baselineK = 10000, disturbanceK = 1000000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
 A_pout <- mean.data.frame(A_pout, burnin = 250, iteration = 2)
 A_pulse <-  cbind.data.frame(temp$dts[250:last(A_pout$timesteps)], A_pout, rep("A", length(A_pout$mean.abund)))
-B_pout <- Bmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
+B_pout <- Bmodel(discharge, temp, baselineK = 10000, disturbanceK = 1000000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
 B_pout <- mean.data.frame(B_pout, burnin = 250, iteration = 2)
 B_pulse <-  cbind.data.frame(temp$dts[250:last(B_pout$timesteps)], B_pout, rep("B", length(B_pout$mean.abund)))
-C_pout <- Cmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
+C_pout <- Cmodel(discharge, temp, baselineK = 10000, disturbanceK = 1000000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
 C_pout <- mean.data.frame(C_pout, burnin = 250, iteration = 2)
 C_pulse <-  cbind.data.frame(temp$dts[250:last(C_pout$timesteps)], C_pout, rep("C", length(C_pout$mean.abund)))
-D_pout <- Dmodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
+D_pout <- Dmodel(discharge, temp, baselineK = 10000, disturbanceK = 1000000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature))
 D_pout <- mean.data.frame(D_pout, burnin = 250, iteration = 2)
 D_pulse <-  cbind.data.frame(temp$dts[250:last(D_pout$timesteps)], D_pout, rep("D", length(D_pout$mean.abund)))
 colnames(A_pulse) <- c("Date", "timesteps", "Abundance", "sd", "se", "Taxa")
@@ -87,6 +87,9 @@ colnames(D_pulse) <- c("Date", "timesteps", "Abundance", "sd", "se", "Taxa")
 
 pulse <- rbind(A_pulse, B_pulse, C_pulse, D_pulse)
 pulse$Date <- as.Date(pulse$Date)
+
+threeyear <- subset(pulse, Date >= "2034-01-01" & Date <= "2037-12-31")
+
 pulse <- subset(pulse, Date >= "2035-01-01" & Date <= "2035-12-31")
 # 
 # max(B_pulse$Abundance[(which(B_pulse$Date == selected_date)):(which(B_pulse$Date == selected_date) + 6)])
