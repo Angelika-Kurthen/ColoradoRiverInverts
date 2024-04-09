975fdacb6101a5b5877738b21cc275936a476031
Angelika Kurthen <kurthena@oregonstate.edu>
2024-04-09 11:06:29 -0700

    code to do NZMS n mix model

--- NMix_HPC.R
+++ NMix_HPC.R
@@ -1,51 +1,77 @@
 ##################
 # NZMS N Mixture
 #################
-#library(dataRetrieval, lib.loc = "/home/ib/kurthena/R_libs/4.2.1")
-library(unmarked, lib.loc = "/home/ib/kurthena/R_libs/4.2.1")
+
+source()
+library(unmarked)
+library(ubms)
+
+# plot abundance over time
+
+drift.data.total <- readDB(gear = "Drift", type = "Sample", updater = TRUE)
+
+drift.LF <- drift.data.total[which(drift.data.total$RiverMile >= -6 & drift.data.total$RiverMile <= 0),]
+
+NZMS.samp.LF <- sampspec(samp = drift.LF, species = "NZMS", stats = T)
+NZMS.samp <- merge(NZMS.samp.LF$Statistics, NZMS.samp.LF$Samples, by = "BarcodeID", all = T)
+NZMS.samp <- NZMS.samp[which(NZMS.samp$GearID == 4),] 
+NZMS.samp$Density <- NZMS.samp$CountTotal/NZMS.samp$Volume
+
+NZMS.samp <- merge(NZMS.samp, discharge[, 3:4], by = "Date")
+NZMS.samp$Density <- (NZMS.samp$Density/(9e-15 *(NZMS.samp$X_00060_00003 * 0.02831683)^4.1))
+write.csv(NZMS.samp, "NZMSsamp.csv")
 
 
-# HPC and dataRetrieval don't like each other
+NZMS.samp <- aggregate(NZMS.samp$Density, list(NZMS.samp$Date), FUN = mean)
 
-#discharge <- readNWISdv("09380000", "00060", "2007-10-01", "2023-05-01")
-#flow.magnitude <- TimestepDischarge(discharge, 85000)
+means <- vector()
+for (i in 1:length(temps$dts)){
+  d <- NZMS.samp[which(NZMS.samp$Group.1 %within% interval(temps$dts[i], temps$dts[i+1]) == T),]
+  if (is.nan(mean(d$x)) == T) {
+    s = NA
+  } else {
+    s<- mean(d$x)}
+  means <- append(means, s)
+}
 
-write.csv(flow.magnitude, file = "flow.magnitude.csv")
-flow.magnitude <- read.csv(file = "flow.magnitude.csv", header = T)
 
-# temp <- readNWISdv("09380000", "00010", "2007-10-01", "2023-05-01")
-# temps <- TimestepTemperature(temp)
+NZMS.samp.sum <- na.omit(as.data.frame(cbind(as.Date(means.list.NZMS$`temps$dts`, format = "%Y-%m-%d"), means[51:406])))
+NZMS.samp.sum$V1 <- as.Date(NZMS.samp.sum$V1, origin = "1970-01-01")
 
-write.csv(temps, file = "temps.csv")
+
 temps <- read.csv(file = "temps.csv", header = T)
-
 
 NZMS.samp <- read.csv("NZMSsamp.csv", header = T)
 
 # need to fit our data to an unmarked frame
 
-# observations need to be in MxJ matrix, where M is # of sites and J is max number of obs per site
-
+# observations need to be in RxJ matrix, where R is # of sites and J is max number of obs per site
 # we measured over temps$dts (those are our timesteps)
 
-max_visits <- vector()
+max_visits <- vector() # vector to put max obs per site
 means <- vector()
-se <- vector()
-for (i in 1:length(temps$dts)){
+for (i in 1:length(temps$dts)){  # cycle through all the 14 day timesteps that we have model output for
+  # pull abundances between each each timestep
   d <- NZMS.samp[which(NZMS.samp$Date >= temps$dts[i] & NZMS.samp$Date < temps$dts[i+1]), ]
-  max_visits[i] <- length(d$Date)
-  means[i] <- mean(d$Density)
-  se[i] <- sd(d$Density)/(length(d$Density))
+  max_visits[i] <- length(d$Date) # number of observations
+  means[i] <- mean(d$CountTotal) # means
 }
 
-df <- cbind.data.frame(temps$dts, means, se)
+# make data frame with the timestep, the mean, and standard error
+df <- cbind.data.frame(temps$dts, means)
+# remove anythig where there are NA values of Density (means that volume or count is missing)
 df <- df[!is.na(df$means), ]
 
+# define our RxJ matrix
 R <- length(temps$dts)
 J <- max(max_visits)
-
 site_mat <- matrix(data = NA, nrow = R, ncol = J)
 dens_mat <- matrix(data = NA, nrow = R, ncol = J)
+
+# make vector for flows at each timestep
+# make RxJ matrix full of densities
+# make RxJ matrix full of raw counts
+# make RxJ matrix full of volumes sampled for each abundance
 flows <- vector()
 volumes <- matrix(data = NA, nrow = R, ncol = J)
 for (i in 1:length(temps$dts)){
@@ -57,42 +83,42 @@ for (i in 1:length(temps$dts)){
 }
 
 # we need to remove all timesteps that are just NAs
-# first identify all the timsteps that don't have data (so we can match them up later)
 nodata <- which(is.na(site_mat[,1]))
-length(temps$dts) - length(nodata)
+# first identify all the timsteps that don't have data (so we can match them up later)
 site_mat <- as.matrix(site_mat[-nodata,])
 dens_mat <- as.matrix(dens_mat[-nodata, ])
 flows <- as.data.frame(flows[-nodata])
 volumes <- as.matrix(volumes[-nodata, ])
 dimnames(volumes) <- list(temps$dts[-nodata], seq(1:48))
-
-
 volumes <- list(volumes)
 names(volumes) <- c("vol")
 
+# convert organized data into unmarked dataframe format, with flow as site covariate and volume of sample as observation covariate
 dat <- unmarkedFramePCount(y = site_mat, siteCovs = flows, obsCovs = volumes)
-dat_dens <- unmarkedFramePCount(y = dens_mat, siteCovs = flows, obsCovs = volumes)
 
-# Model with no covariates or random effect on abundance
-# mod_null <- stan_pcount(~ 1 ~1, dat, K=10453,
-#                         chains=3, iter=2000, seed=123, prior_coef_det = logistic(0.69, 0.97), log_lik = F)
-# mod_vol <- stan_pcount(~scale(vol)~1, dat, K = 10453, chains = 3, iter = 2000, seed= 123,prior_coef_det = logistic(0, 1), log_lik= F)
+# pcount is N-mixture model to estimate counts at each site
+# we are doing space-time approximation
 
-mod_um_vol <- pcount(~ vol ~1 , dat, K = 10453)
-mod_off_vol <- pcount(~ offset(vol) ~ 1, dat, K = 10453)
+# we want to offset our count data by volume (since volumes vary slighlty by each sample)
+# these are basically our three different hypotheses
+#Poisson mixture
+mod_um_vol_P <- pcount(~ 1 + offset(vol) ~1 , dat, K = 11000, mixture = "P")
+# Neg Binom mixture
+mod_um_vol_NB <- pcount(~ 1 + offset(vol) ~1 , dat, K = 11000, mixture = "NB")
+# Zero Inflated Poisson mixture
+mod_um_vol_ZIP <- pcount(~ 1 + offset(vol) ~1 , dat, K =11000, mixture = "ZIP")
 
-mod_um_vol_NB <- pcount(~ 1 + offset(vol) ~1 , dat, K = 10453, mixture = "NB")
-mod_um_vol_ZIP <- pcount(~ 1 + offset(vol) ~1 , dat, K = 10453, mixture = "ZIP")
-mod_um_dens <- pcount(~1 ~1, dat_dens, K = 1176244)
-mod_um_dens_NB <- pcount(~1 ~1, dat_dens, K = 1176244, mixture = "NB")
-mod_um_dens_ZIP <- pcount(~1 ~1, dat_dens, K = 1176244, mixture = "ZIP")
-
-
-#dens_mod_mull <- stan_pcount(~1 ~1, dat_dens, K = 1176244, chains = 3, iter = 2000, seed = 123, log_lik = T)
-fm <- fitList(mod_um_dens, mod_um_dens_NB)
+# now we want to compare the models
+fm <- fitList(mod_um_vol_P, mod_um_vol_NB, mod_um_vol_ZIP)
 modSel(fm)
 # Model selectionfit
-# Function returning three fit-statistics.
+# suggests that NB is best model
+
+
+# use parboot function to imulate datasets from a fitted model, refit the model, 
+#and generate a sampling distribution for a user-specified fit-statistic
+
+# Function returning three fit-statistics
 fitstats <- function(fm, na.rm=TRUE) {
   observed <- getY(fm@data)
   expected <- fitted(fm)
@@ -103,65 +129,47 @@ fitstats <- function(fm, na.rm=TRUE) {
   out <- c(SSE=sse, Chisq=chisq, freemanTukey=freeTuke)
   return(out)
 }
-pb <- parboot(mod_um_vol, fitstats, nsim=25, report=1)
-pb_dens <- parboot(mod_um_dens, fitstats, nsim = 25, report = 1)
+
+pb <- parboot(mod_um_vol_NB, fitstats, nsim=25, report=1)
+pb
+
 pb_plot_mod_vol <- plot(pb, main="")
-pb_plot_mod_dens <- plot(pb_dens, main = "")
-png(filename = "pb_plot_mod_vol.png")
 plot(pb_plot_mod_vol)
-dev.off()
-png(filename = "pb_plot_mod_dens.png")
-plot(pb_plot_mod_dens)
-dev.off()
-# Finite-sample inference for a derived parameter.
-# Population size in sampled area
-Nhat <- function(fm) {
-  sum(bup(ranef(fm, K=50)))
-}
-set.seed(345)
 
+pb_plot_mod_dens <- plot(pb_dens, main = "")
 
-
-pb_dens.N <- parboot(mod_um_vol_NB, fitstats, nsim=25, report=5)
-
-
-
-pb_vol.N <- parboot(mod_um_NB, Nhat, nsim = 25, report = 5)
 # Compare to empirical Bayes confidence intervals
-colSums(confint(ranef(mod_um_vol_NB, K=10453)))
-colSums(confint(ranef(mod_um_dens, K = 50)))
-
+colSums(confint(ranef(mod_um_vol_NB, K=11000)))
 # Estimates of conditional abundance distribution at each site
-re <- ranef(mod_um_dens_NB)
-re_dens <- ranef(mod_um_vol_NB)
+re <- ranef(mod_um_vol_NB)
 # Best Unbiased Predictors
-bup(re, stat="mean") # Posterior mean
-bup(re, stat="mode") # Posterior mode
-est <- bup(re) # estimated population size
+est <- bup(re) # estimated population size at each timestep
+conf <- as.data.frame(confint(re, level=0.95)) # 90% CI
+# get matching dates that match with non-NA estimations
+date <- df$`temps$dts`[!is.nan(est)]
+conf <- (conf[!is.nan(est),])
 
+
+# remove the NAs from out estimation 
 est <- est[!is.nan(est)]
-obs <- df$means[!is.nan(est)]
-cor(est, obs)
-
-mutate(df$means, dens =  )
+# check for temporal autocorrelation, there is some at 8 and 9, and 17?
+acf(est)
 
 
-confint(re, level=0.95) # 90% CI
+estimate <- cbind.data.frame(date, est, conf)
+estimate$date <- as.Date(estimate$date)
 
-# Plots
-plot(re)
+ggplot(data = estimate, aes(x = (date), y = est))+
+    geom_line(size = 1, alpha = 0.8)+
+    geom_point(size = 2, alpha = 0.5)
+    #geom_ribbon(aes(ymin = `2.5%`, ymax = `97.5%` ), alpha = 0.2)
 
-# Best Unbiased Predictors
-bup(re_dens, stat="mean") # Posterior mean
-bup(re_dens, stat="mode") # Posterior mode
-bup(re_dens) # estimated population size
-confint(re_dens, level=0.9) # 90% CI
-# Plots
-plot(re, subset=site %in% 1:25)
-plot(re, subset=site %in% 26:50)
-plot(re, subset= site %in% 51:100)
-plot(re, subset = site %in% 101:142)
-     
+
+
+
+#
+
+
 #dens_mod_mull <- stan_pcount(~1 ~1, dat_dens, K = 1176244, chains = 3, iter = 2000, seed = 123, log_lik = T)
 
 # occupancy to get global p 
