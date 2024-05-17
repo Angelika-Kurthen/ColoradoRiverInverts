521832387940034dcea62a784d5aaac93a017f30
Angelika Kurthen <kurthena@oregonstate.edu>
2024-05-17 09:12:45 -0700

    adjusted temperatures for no growth

--- CHIR_1sp_Model.R
+++ CHIR_1sp_Model.R
@@ -198,8 +198,8 @@ CHIRmodel <- function(flow.data, temp.data, baselineK, disturbanceK, Qmin, extin
       # in this function, we assume that if below the min temp threshold (9) slow maturation
       # if above the max temp threshold (30), no one remains more than 1 timestep in each stage (fast maturation, small growth)
 
-  if (10 > temps$Temperature[t-1]) {
-    P1 <- (1-(1/4)) #* TempSurvival[t-1]
+  if (9 > temps$Temperature[t-1]) {
+    P1 <- (1-(1/4)) * TempSurvival[t-1]
     P2 <- P1 
     G1 <- 0 * TempSurvival[t-1]
     G2 <- 0 * TempSurvival[t-1]
@@ -207,20 +207,20 @@ CHIRmodel <- function(flow.data, temp.data, baselineK, disturbanceK, Qmin, extin
 if (temps$Temperature[t-1] > 30){
     P1 <- 0
     P2 <- 0
-    G1 <- 0.42 * TempSurvival[t-1]
-    G2 <- 0.42 * TempSurvival[t-1]
+    G1 <- 0.35 * TempSurvival[t-1]
+    G2 <- 0.775 * TempSurvival[t-1]
   }
 
-      if (10 <= temps$Temperature[t-1] & temps$Temperature[t-1] <= 30 & (is.na(emergetime[t-1]) == F)){
-        G1 <- (0.42/((emergetime[t-1])/2)) * TempSurvival[t-1]
-        G2 <- (0.42/((emergetime[t-1])/2)) * TempSurvival[t-1]
+      if (9 <= temps$Temperature[t-1] & temps$Temperature[t-1] <= 30 & (is.na(emergetime[t-1]) == F)){
+        G1 <- (0.35/((emergetime[t-1])/2)) * TempSurvival[t-1]
+        G2 <- (0.775/((emergetime[t-1])/2)) * TempSurvival[t-1]
         P1 <- (1-(1/((emergetime[t-1])/2))) * TempSurvival[t-1]
         P2 <- P1
       }
-      if (10 <= temps$Temperature[t-1] & temps$Temperature[t-1] <= 30 & (is.na(emergetime[t-1]) == T)) {
-        G1 <- (0.42*((-0.136 * temps$Temperature[t-1]) + 5.088)) * TempSurvival[t-1]
+      if (9 <= temps$Temperature[t-1] & temps$Temperature[t-1] <= 30 & (is.na(emergetime[t-1]) == T)) {
+        G1 <- (0.35*((-0.136 * temps$Temperature[t-1]) + 5.088)) * TempSurvival[t-1]
         P1 <- (1-(1/((-0.136 * temps$Temperature[t-1]) + 5.088))) * TempSurvival[t-1]
-        G2 <- (0.42*((-0.136 * temps$Temperature[t-1]) + 5.088)) * TempSurvival[t-1]
+        G2 <- (0.775*((-0.136 * temps$Temperature[t-1]) + 5.088)) * TempSurvival[t-1]
         P2 <- P1
       }
       
