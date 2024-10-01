578493c7c07fa2cf09fdb47185a0558a2baaff65
Angelika Kurthen <kurthena@oregonstate.edu>
2024-09-30 19:33:44 -0700

    cleaning

--- CodeforNumpy.R
+++
@@ -1,9 +0,0 @@
-# code for exporting into numpy 
-install.packages("reticulate")
-library(reticulate)
-np = import("numpy")
-a <- np$array(outarray)
-pa <- r_to_py(a)
-py_save_object(pa, "test.npy")
-np$load
-np$load("test.npy", allow_pickle=T)
